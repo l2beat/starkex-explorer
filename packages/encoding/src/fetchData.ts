d1dfc35cb436e0d9edc5563ff07e5aba5ef15168
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-12-10 13:49:12 +0100

    Get memory page events

--- packages/encoding/src/fetchData.ts
+++ packages/encoding/src/fetchData.ts
@@ -1,5 +1,7 @@
 import { providers, utils } from 'ethers'
+
 import { getGpsVerifiers } from './getGpsVerifiers'
+import { getMemoryPageEvents } from './getMemoryPageEvents'
 
 const PERPETUAL_ADDRESS = '0xD54f502e184B6B739d7D27a6410a67dc462D69c8'
 
@@ -17,7 +19,9 @@ export async function getStateTransitionFacts(blockHash: string) {
   const events = logs.map((log) => PERPETUAL_ABI.parseLog(log))
   const facts = events.map((event) => event.args.stateTransitionFact)
   const verifiers = await getGpsVerifiers(provider)
-  console.log(verifiers)
+  const latest = (await provider.getBlockNumber()) - 100
+  const mpEvents = await getMemoryPageEvents(provider, 11813207, 13777355)
+  console.log(mpEvents)
 }
 
 const GPS_VERIFIER_ABI = new utils.Interface([
--- packages/encoding/src/index.ts
+++ packages/encoding/src/index.ts
@@ -4,4 +4,6 @@ export function decode(value: string) {
   return parseInt(value)
 }
 
-getStateTransitionFacts('0x46c212912be05a090a9300cf87fd9434b8e8bbca15878d070ba83375a5dbaebd')
+getStateTransitionFacts(
+  '0x46c212912be05a090a9300cf87fd9434b8e8bbca15878d070ba83375a5dbaebd'
+)
