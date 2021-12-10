0025d5a3cd6e385271be9b0e7a0d5c10c5b9e600
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-12-10 13:19:16 +0100

    Get GPS verifier addresses

--- packages/encoding/src/fetchData.ts
+++ packages/encoding/src/fetchData.ts
@@ -1,4 +1,5 @@
 import { providers, utils } from 'ethers'
+import { getGpsVerifiers } from './getGpsVerifiers'
 
 const PERPETUAL_ADDRESS = '0xD54f502e184B6B739d7D27a6410a67dc462D69c8'
 
@@ -15,5 +16,10 @@ export async function getStateTransitionFacts(blockHash: string) {
   })
   const events = logs.map((log) => PERPETUAL_ABI.parseLog(log))
   const facts = events.map((event) => event.args.stateTransitionFact)
-  console.log(facts)
+  const verifiers = await getGpsVerifiers(provider)
+  console.log(verifiers)
 }
+
+const GPS_VERIFIER_ABI = new utils.Interface([
+  'event LogMemoryPagesHashes(bytes32 factHash, bytes32[] pagesHashes)',
+])
