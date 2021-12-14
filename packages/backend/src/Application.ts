a8d94e61ed5e70eb5ba7ac987aa1eecce48f1749
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-12-14 14:26:44 +0100

    Add a basic status api

--- packages/backend/src/Application.ts
+++ packages/backend/src/Application.ts
@@ -1,3 +1,5 @@
+import { ApiServer } from './api/ApiServer'
+import { createStatusRouter } from './api/routers/StatusRouter'
 import { Config } from './config'
 import { Logger } from './tools/Logger'
 
@@ -9,10 +11,17 @@ export class Application {
 
     const logger = new Logger(config.logger)
 
+    /* - - - - - API - - - - - */
+
+    const apiServer = new ApiServer(config.port, logger, [createStatusRouter()])
+
     /* - - - - - START - - - - - */
 
     this.start = async () => {
       logger.for(this).info('Starting')
+
+      await apiServer.listen()
+
       logger.for(this).info('Started')
     }
   }
