699804de381e5ea41f170ad3a3f7d5fab44adecf
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-12-14 14:31:57 +0100

    Add a script for running in production

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -6,8 +6,12 @@ const env = process.env.NODE_ENV === 'production' ? 'production' : 'local'
 try {
   const config = getConfig(env)
   const app = new Application(config)
-  app.start()
+  app.start().catch(onError)
 } catch (e) {
+  onError(e)
+}
+
+function onError(e: unknown) {
   console.error(e)
   process.exit(1)
 }
