3d4c86895236b0789e486dd14544e4461994833b
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-12-21 13:27:08 +0100

    Remove the time based test

--- packages/crypto/test/pedersen.test.ts
+++ packages/crypto/test/pedersen.test.ts
@@ -13,31 +13,5 @@ describe('pedersen', () => {
     )
   })
 
-  async function getExecutionTime(fn: () => Promise<unknown>) {
-    const start = Date.now()
-    await fn()
-    return Date.now() - start
-  }
-
-  it('hashes values in parallel', async () => {
-    async function hash() {
-      return pedersen(
-        'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-        'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
-      )
-    }
-
-    async function hashMany(n: number) {
-      return Promise.all(new Array(n).fill(0).map(hash))
-    }
-
-    // initialize
-    await hashMany(10)
-
-    const shortTime = await getExecutionTime(() => hashMany(1))
-    const longTime = await getExecutionTime(() => hashMany(10))
-    expect(longTime).to.be.lessThan(shortTime * 4)
-  })
-
   after(terminateWorkerPool)
 })
