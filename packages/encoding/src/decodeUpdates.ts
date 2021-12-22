14dbe8c8050bc1ce1dfc7a41b4979a4293e5d8f6
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-12-22 14:39:40 +0100

    Use ByteReader

--- packages/encoding/src/decodeUpdates.ts
+++ packages/encoding/src/decodeUpdates.ts
@@ -1,5 +1,5 @@
 import { decodeAssetId } from './assetId'
-import { DecodingError } from './DecodingError'
+import { ByteReader } from './ByteReader'
 import {
   AssetBalance,
   FundingEntry,
@@ -11,40 +11,36 @@ import {
 const MIN_INT = -(2n ** 63n)
 
 export function decodeUpdates(data: string[]): OnChainData {
-  let position = 0
+  const reader = new ByteReader(data.join(''))
 
-  const fundingEntriesLength = hexToSafeInt(data[position++])
+  const fundingEntriesLength = reader.readNumber(32)
   const funding: FundingEntry[] = []
   for (let i = 0; i < fundingEntriesLength; i++) {
-    const fundingIndicesLength = hexToSafeInt(data[position++])
-
+    const fundingIndicesLength = reader.readNumber(32)
     const indices: FundingIndex[] = []
-    const end = position + fundingIndicesLength * 2
-    for (; position < end; position += 2) {
-      indices.push({
-        assetId: hexToAssetId(data[position]),
-        value: hexToBigInt(data[position + 1]) + MIN_INT,
-      })
+    for (let i = 0; i < fundingIndicesLength; i++) {
+      reader.skip(17)
+      const assetId = decodeAssetId(reader.read(15))
+      const value = reader.readBigInt(32) + MIN_INT
+      indices.push({ assetId, value })
     }
-
-    const timestamp = hexToBigInt(data[position++])
+    const timestamp = reader.readBigInt(32)
     funding.push({ indices, timestamp })
   }
 
   const positions: PositionUpdate[] = []
-  while (position < data.length) {
-    const stateValuesLength = hexToSafeInt(data[position++])
-    const positionId = hexToBigInt(data[position++])
-    const publicKey = hexToString(data[position++])
-    const collateralBalance = hexToBigInt(data[position++]) + MIN_INT
-    const fundingTimestamp = hexToBigInt(data[position++])
+  while (!reader.isAtEnd()) {
+    const stateValuesLength = reader.readNumber(32)
+    const positionId = reader.readBigInt(32)
+    const publicKey = '0x' + reader.read(32)
+    const collateralBalance = reader.readBigInt(32) + MIN_INT
+    const fundingTimestamp = reader.readBigInt(32)
 
     const balances: AssetBalance[] = []
-    const end = position + stateValuesLength - 4
-    for (; position < end; position++) {
-      const item = data[position]
-      const assetId = hexToAssetId(item?.slice(72 / 4, (72 + 120) / 4))
-      const balance = hexToBigInt(item?.slice((72 + 120) / 4)) + MIN_INT
+    for (let i = 0; i < stateValuesLength - 4; i++) {
+      reader.skip(9)
+      const assetId = decodeAssetId(reader.read(15))
+      const balance = reader.readBigInt(8) + MIN_INT
       balances.push({ assetId, balance })
     }
 
@@ -58,35 +54,4 @@ export function decodeUpdates(data: string[]): OnChainData {
   }
 
   return { funding, positions }
-}
-
-export function hexToAssetId(value: string | undefined) {
-  checkIntegrity(value)
-  const last15bytes = value.substring(value.length - 30)
-  return decodeAssetId(last15bytes)
-}
-
-function hexToBigInt(value: string | undefined) {
-  checkIntegrity(value)
-  return BigInt('0x' + value)
-}
-
-function hexToSafeInt(value: string | undefined) {
-  checkIntegrity(value)
-  const number = parseInt(value, 16)
-  if (number > Number.MAX_SAFE_INTEGER) {
-    throw new DecodingError('Number too large')
-  }
-  return number
-}
-
-function hexToString(value: string | undefined) {
-  checkIntegrity(value)
-  return '0x' + value
-}
-
-function checkIntegrity<T>(value: T | undefined): asserts value is T {
-  if (value === undefined) {
-    throw new DecodingError('Went out of bounds')
-  }
 }
