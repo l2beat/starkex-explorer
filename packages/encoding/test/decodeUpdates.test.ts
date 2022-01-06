fff061750a923cc8a493873a939c50ff2d3349a5
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-01-06 12:15:56 +0100

    Add tests for readPositionUpdate

--- packages/encoding/test/decodeUpdates.test.ts
+++ packages/encoding/test/decodeUpdates.test.ts
@@ -1,11 +1,10 @@
 import { expect } from 'chai'
 
+import { MIN_INT } from '../src/constants'
 import { encodeAssetId } from '../src/decodeAssetId'
 import { decodeUpdates } from '../src/decodeUpdates'
 import { DecodingError } from '../src/DecodingError'
 import { ByteWriter } from './ByteWriter'
-
-const OFFSET = 2n ** 63n
 
 describe('decodeUpdates', () => {
   it('fails for empty data', () => {
@@ -15,238 +14,47 @@ describe('decodeUpdates', () => {
     )
   })
 
-  it('decodes a single entry with a single index', () => {
-    const writer = new ByteWriter()
-      .writeNumber(1, 32) // single entry
-      .writeNumber(1, 32) // single index
-      .writePadding(17)
-      .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 1n, 32) // funding index = 1
-      .writeNumber(456, 32) // timestamp
-    expect(decodeUpdates(writer.getBytes())).to.deep.equal({
-      funding: [
-        {
-          indices: [{ assetId: 'ETH-9', value: 1n }],
-          timestamp: 456n,
-        },
-      ],
-      positions: [],
-    })
-  })
-
-  it('decodes a single entry with a multiple indices', () => {
-    const writer = new ByteWriter()
-      .writeNumber(1, 32) // single entry
-      .writeNumber(3, 32) // 3 indices
-      .writePadding(17)
-      .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 100n, 32) // funding index = 100
-      .writePadding(17)
-      .write(encodeAssetId('BTC-10'))
-      .writeNumber(OFFSET - 200n, 32) // funding index = -200
-      .writePadding(17)
-      .write(encodeAssetId('ABC-1'))
-      .writeNumber(OFFSET, 32) // funding index = 0
-      .writeNumber(456, 32) // timestamp
-    expect(decodeUpdates(writer.getBytes())).to.deep.equal({
-      funding: [
-        {
-          indices: [
-            { assetId: 'ETH-9', value: 100n },
-            { assetId: 'BTC-10', value: -200n },
-            { assetId: 'ABC-1', value: 0n },
-          ],
-          timestamp: 456n,
-        },
-      ],
-      positions: [],
-    })
-  })
-
-  it('decodes multiple entries with multiple indices', () => {
-    const writer = new ByteWriter()
-      .writeNumber(2, 32) // 2 entries
-      .writeNumber(3, 32) // 3 indices
-      .writePadding(17)
-      .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 100n, 32) // funding index = 100
-      .writePadding(17)
-      .write(encodeAssetId('BTC-10'))
-      .writeNumber(OFFSET - 200n, 32) // funding index = -200
-      .writePadding(17)
-      .write(encodeAssetId('ABC-1'))
-      .writeNumber(OFFSET, 32) // funding index = 0
-      .writeNumber(456, 32) // timestamp
-      .writeNumber(2, 32) // 2 indices
-      .writePadding(17)
-      .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 1n, 32) // funding index = 1
-      .writePadding(17)
-      .write(encodeAssetId('BTC-10'))
-      .writeNumber(OFFSET + 2n, 32) // funding index = 2
-      .writeNumber(789, 32) // timestamp
-    expect(decodeUpdates(writer.getBytes())).to.deep.equal({
-      funding: [
-        {
-          indices: [
-            { assetId: 'ETH-9', value: 100n },
-            { assetId: 'BTC-10', value: -200n },
-            { assetId: 'ABC-1', value: 0n },
-          ],
-          timestamp: 456n,
-        },
-        {
-          indices: [
-            { assetId: 'ETH-9', value: 1n },
-            { assetId: 'BTC-10', value: 2n },
-          ],
-          timestamp: 789n,
-        },
-      ],
-      positions: [],
-    })
-  })
-
-  it('decodes a single position with no values', () => {
-    const writer = new ByteWriter()
-      .writeNumber(0, 32) // 0 entries
-      .writeNumber(4, 32) // 0 values
-      .writeNumber(123, 32) // positionId
-      .write('1234abcd'.repeat(8)) // publicKey
-      .writeNumber(OFFSET + 10n, 32) // collateralBalance
-      .writeNumber(456, 32) // fundingTimestamp
-    expect(decodeUpdates(writer.getBytes())).to.deep.equal({
-      funding: [],
-      positions: [
-        {
-          positionId: 123n,
-          publicKey: '0x' + '1234abcd'.repeat(8),
-          collateralBalance: 10n,
-          fundingTimestamp: 456n,
-          balances: [],
-        },
-      ],
-    })
-  })
-
-  it('decodes a single position with multiple values', () => {
-    const writer = new ByteWriter()
-      .writeNumber(0, 32) // 0 entries
-      .writeNumber(4 + 2, 32) // 2 values
-      .writeNumber(123, 32) // positionId
-      .write('1234abcd'.repeat(8)) // publicKey
-      .writeNumber(OFFSET + 10n, 32) // collateralBalance
-      .writeNumber(456, 32) // fundingTimestamp
-      .writePadding(9)
-      .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 50n, 8)
-      .writePadding(9)
-      .write(encodeAssetId('BTC-10'))
-      .writeNumber(OFFSET + 20n, 8)
-    expect(decodeUpdates(writer.getBytes())).to.deep.equal({
-      funding: [],
-      positions: [
-        {
-          positionId: 123n,
-          publicKey: '0x' + '1234abcd'.repeat(8),
-          collateralBalance: 10n,
-          fundingTimestamp: 456n,
-          balances: [
-            { assetId: 'ETH-9', balance: 50n },
-            { assetId: 'BTC-10', balance: 20n },
-          ],
-        },
-      ],
-    })
-  })
-
-  it('decodes multiple positions with multiple values', () => {
-    const writer = new ByteWriter()
-      .writeNumber(0, 32) // 0 entries
-      .writeNumber(4 + 2, 32) // 2 values
-      .writeNumber(123, 32) // positionId
-      .write('1234abcd'.repeat(8)) // publicKey
-      .writeNumber(OFFSET + 10n, 32) // collateralBalance
-      .writeNumber(456, 32) // fundingTimestamp
-      .writePadding(9)
-      .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 50n, 8)
-      .writePadding(9)
-      .write(encodeAssetId('BTC-10'))
-      .writeNumber(OFFSET + 20n, 8)
-      .writeNumber(4 + 1, 32) // 1 value
-      .writeNumber(124, 32) // positionId
-      .write('deadbeef'.repeat(8)) // publicKey
-      .writeNumber(OFFSET + 33n, 32) // collateralBalance
-      .writeNumber(457, 32) // fundingTimestamp
-      .writePadding(9)
-      .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 66n, 8)
-    expect(decodeUpdates(writer.getBytes())).to.deep.equal({
-      funding: [],
-      positions: [
-        {
-          positionId: 123n,
-          publicKey: '0x' + '1234abcd'.repeat(8),
-          collateralBalance: 10n,
-          fundingTimestamp: 456n,
-          balances: [
-            { assetId: 'ETH-9', balance: 50n },
-            { assetId: 'BTC-10', balance: 20n },
-          ],
-        },
-        {
-          positionId: 124n,
-          publicKey: '0x' + 'deadbeef'.repeat(8),
-          collateralBalance: 33n,
-          fundingTimestamp: 457n,
-          balances: [{ assetId: 'ETH-9', balance: 66n }],
-        },
-      ],
-    })
-  })
-
   it('decodes multiple entries and positions', () => {
     const writer = new ByteWriter()
       .writeNumber(2, 32) // 2 entries
       .writeNumber(3, 32) // 3 indices
       .writePadding(17)
       .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 100n, 32) // funding index = 100
+      .writeNumber(100n - MIN_INT, 32) // funding index = 100
       .writePadding(17)
       .write(encodeAssetId('BTC-10'))
-      .writeNumber(OFFSET - 200n, 32) // funding index = -200
+      .writeNumber(-200n - MIN_INT, 32) // funding index = -200
       .writePadding(17)
       .write(encodeAssetId('ABC-1'))
-      .writeNumber(OFFSET, 32) // funding index = 0
+      .writeNumber(0n - MIN_INT, 32) // funding index = 0
       .writeNumber(456, 32) // timestamp
       .writeNumber(2, 32) // 2 indices
       .writePadding(17)
       .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 1n, 32) // funding index = 1
+      .writeNumber(1n - MIN_INT, 32) // funding index = 1
       .writePadding(17)
       .write(encodeAssetId('BTC-10'))
-      .writeNumber(OFFSET + 2n, 32) // funding index = 2
+      .writeNumber(2n - MIN_INT, 32) // funding index = 2
       .writeNumber(789, 32) // timestamp
       .writeNumber(4 + 2, 32) // 2 values
       .writeNumber(123, 32) // positionId
       .write('1234abcd'.repeat(8)) // publicKey
-      .writeNumber(OFFSET + 10n, 32) // collateralBalance
+      .writeNumber(10n - MIN_INT, 32) // collateralBalance
       .writeNumber(456, 32) // fundingTimestamp
       .writePadding(9)
       .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 50n, 8)
+      .writeNumber(50n - MIN_INT, 8)
       .writePadding(9)
       .write(encodeAssetId('BTC-10'))
-      .writeNumber(OFFSET + 20n, 8)
+      .writeNumber(20n - MIN_INT, 8)
       .writeNumber(4 + 1, 32) // 1 value
       .writeNumber(124, 32) // positionId
       .write('deadbeef'.repeat(8)) // publicKey
-      .writeNumber(OFFSET + 33n, 32) // collateralBalance
+      .writeNumber(33n - MIN_INT, 32) // collateralBalance
       .writeNumber(457, 32) // fundingTimestamp
       .writePadding(9)
       .write(encodeAssetId('ETH-9'))
-      .writeNumber(OFFSET + 66n, 8)
+      .writeNumber(66n - MIN_INT, 8)
     expect(decodeUpdates(writer.getBytes())).to.deep.equal({
       funding: [
         {
---
+++ packages/encoding/test/readPositionUpdate.test.ts
@@ -0,0 +1,57 @@
+import { expect } from 'chai'
+
+import { DecodingError } from '../src'
+import { MIN_INT } from '../src/constants'
+import { encodeAssetId } from '../src/decodeAssetId'
+import { readPositionUpdate } from '../src/readPositionUpdate'
+import { ByteWriter } from './ByteWriter'
+import { readToDecode } from './readToDecode'
+
+describe('readPositionUpdate', () => {
+  const decode = readToDecode(readPositionUpdate)
+
+  it('fails for empty data', () => {
+    expect(() => decode('')).to.throw(DecodingError, 'Went out of bounds')
+  })
+
+  it('decodes a position with no values', () => {
+    const writer = new ByteWriter()
+      .writeNumber(4, 32) // 0 values
+      .writeNumber(123, 32) // positionId
+      .write('1234abcd'.repeat(8)) // publicKey
+      .writeNumber(10n - MIN_INT, 32) // collateralBalance
+      .writeNumber(456, 32) // fundingTimestamp
+    expect(decode(writer.getBytes())).to.deep.equal({
+      positionId: 123n,
+      publicKey: '0x' + '1234abcd'.repeat(8),
+      collateralBalance: 10n,
+      fundingTimestamp: 456n,
+      balances: [],
+    })
+  })
+
+  it('decodes a position with multiple values', () => {
+    const writer = new ByteWriter()
+      .writeNumber(4 + 2, 32) // 2 values
+      .writeNumber(123, 32) // positionId
+      .write('1234abcd'.repeat(8)) // publicKey
+      .writeNumber(10n - MIN_INT, 32) // collateralBalance
+      .writeNumber(456, 32) // fundingTimestamp
+      .writePadding(9)
+      .write(encodeAssetId('ETH-9'))
+      .writeNumber(50n - MIN_INT, 8)
+      .writePadding(9)
+      .write(encodeAssetId('BTC-10'))
+      .writeNumber(20n - MIN_INT, 8)
+    expect(decode(writer.getBytes())).to.deep.equal({
+      positionId: 123n,
+      publicKey: '0x' + '1234abcd'.repeat(8),
+      collateralBalance: 10n,
+      fundingTimestamp: 456n,
+      balances: [
+        { assetId: 'ETH-9', balance: 50n },
+        { assetId: 'BTC-10', balance: 20n },
+      ],
+    })
+  })
+})
