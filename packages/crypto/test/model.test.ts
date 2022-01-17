f352123fa712d5402fe45e41593d16c663634477
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-01-17 15:24:17 +0100

    Migrate new tests

--- packages/crypto/test/model.test.ts
+++ packages/crypto/test/model.test.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai'
+import { expect } from 'earljs'
 
 import { PedersenHash } from '../src'
 
@@ -6,37 +6,43 @@ describe(PedersenHash.name, () => {
   it('supports 0x prefixed strings', () => {
     const value = '0x1234dead'
     const hash = PedersenHash(value)
-    expect(hash).to.equal('1234dead'.padStart(64, '0'))
+    expect(hash).toEqual(
+      '1234dead'.padStart(64, '0') as unknown as PedersenHash
+    )
   })
 
   it('supports unprefixed strings', () => {
     const value = '1234dead'
     const hash = PedersenHash(value)
-    expect(hash).to.equal('1234dead'.padStart(64, '0'))
+    expect(hash).toEqual(
+      '1234dead'.padStart(64, '0') as unknown as PedersenHash
+    )
   })
 
   it('converts values to lowercase', () => {
     const value = '0x1234DEAD'
     const hash = PedersenHash(value)
-    expect(hash).to.equal('1234dead'.padStart(64, '0'))
+    expect(hash).toEqual(
+      '1234dead'.padStart(64, '0') as unknown as PedersenHash
+    )
   })
 
   it('throws for non hex strings', () => {
-    expect(() => PedersenHash('foo')).to.throw(
+    expect(() => PedersenHash('foo')).toThrow(
       TypeError,
       'Value must be a hex string'
     )
   })
 
   it('throws for empty string', () => {
-    expect(() => PedersenHash('')).to.throw(
+    expect(() => PedersenHash('')).toThrow(
       TypeError,
       'Value must be a hex string'
     )
   })
 
   it('throws for a string that is too long', () => {
-    expect(() => PedersenHash('1'.repeat(65))).to.throw(
+    expect(() => PedersenHash('1'.repeat(65))).toThrow(
       TypeError,
       'Value too large'
     )
@@ -44,12 +50,12 @@ describe(PedersenHash.name, () => {
 
   it('supports maximum value', () => {
     const hash = PedersenHash('0' + 'f'.repeat(63))
-    expect(hash).to.equal('0' + 'f'.repeat(63))
+    expect(hash).toEqual(('0' + 'f'.repeat(63)) as unknown as PedersenHash)
   })
 
   describe(PedersenHash.fromNumber.name, () => {
     it('throws for floating points', () => {
-      expect(() => PedersenHash.fromNumber(1.5)).to.throw(
+      expect(() => PedersenHash.fromNumber(1.5)).toThrow(
         TypeError,
         'Value cannot be floating point'
       )
@@ -58,18 +64,18 @@ describe(PedersenHash.name, () => {
     it('throws for too large numbers', () => {
       expect(() =>
         PedersenHash.fromNumber(2 * Number.MAX_SAFE_INTEGER)
-      ).to.throw(TypeError, 'Value too large')
+      ).toThrow(TypeError, 'Value too large')
     })
 
     it('throws for negative numbers', () => {
-      expect(() => PedersenHash.fromNumber(-2)).to.throw(
+      expect(() => PedersenHash.fromNumber(-2)).toThrow(
         TypeError,
         'Value cannot be negative'
       )
     })
 
     it('throws for negative bigints', () => {
-      expect(() => PedersenHash.fromNumber(-2n)).to.throw(
+      expect(() => PedersenHash.fromNumber(-2n)).toThrow(
         TypeError,
         'Value cannot be negative'
       )
@@ -78,25 +84,25 @@ describe(PedersenHash.name, () => {
     it('throws for too large bigints', () => {
       expect(() =>
         PedersenHash.fromNumber(BigInt('0x1' + '0'.repeat(63)))
-      ).to.throw(TypeError, 'Value too large')
+      ).toThrow(TypeError, 'Value too large')
     })
 
     it('converts zero', () => {
-      expect(PedersenHash.fromNumber(0)).to.equal(PedersenHash.ZERO)
-      expect(PedersenHash.fromNumber(0n)).to.equal(PedersenHash.ZERO)
+      expect(PedersenHash.fromNumber(0)).toEqual(PedersenHash.ZERO)
+      expect(PedersenHash.fromNumber(0n)).toEqual(PedersenHash.ZERO)
     })
 
     it('converts 0x1a2b3c', () => {
       const hash = PedersenHash('1a2b3c')
 
-      expect(PedersenHash.fromNumber(0x1a2b3c)).to.equal(hash)
-      expect(PedersenHash.fromNumber(0x1a2b3cn)).to.equal(hash)
+      expect(PedersenHash.fromNumber(0x1a2b3c)).toEqual(hash)
+      expect(PedersenHash.fromNumber(0x1a2b3cn)).toEqual(hash)
     })
 
     it('converts maximum value', () => {
       const hash = PedersenHash('f'.repeat(63))
 
-      expect(PedersenHash.fromNumber(BigInt('0x' + 'f'.repeat(63)))).to.equal(
+      expect(PedersenHash.fromNumber(BigInt('0x' + 'f'.repeat(63)))).toEqual(
         hash
       )
     })
--- packages/state/test/MerkleTree.test.ts
+++ packages/state/test/MerkleTree.test.ts
@@ -1,5 +1,5 @@
 import { PedersenHash } from '@explorer/crypto'
-import { expect } from 'chai'
+import { expect } from 'earljs'
 
 import { InMemoryMerkleStorage } from '../src/InMemoryMerkleStorage'
 import { MerkleTree } from '../src/MerkleTree'
@@ -11,13 +11,13 @@ describe(MerkleTree.name, () => {
     it('can create a tree of height 0', async () => {
       const storage = new InMemoryMerkleStorage()
       const tree = await MerkleTree.create(storage, 0n, PositionState.EMPTY)
-      expect(await tree.hash()).to.equal(await PositionState.EMPTY.hash())
+      expect(await tree.hash()).toEqual(await PositionState.EMPTY.hash())
     })
 
     it('can create a tree of height 64', async () => {
       const storage = new InMemoryMerkleStorage()
       const tree = await MerkleTree.create(storage, 64n, PositionState.EMPTY)
-      expect(await tree.hash()).to.equal(
+      expect(await tree.hash()).toEqual(
         PedersenHash(
           '52ddcbdd431a044cf838a71d194248640210b316d7b1a568997ecad9dec9626'
         )
@@ -31,7 +31,7 @@ describe(MerkleTree.name, () => {
         persisted = values
       }
       const tree = await MerkleTree.create(storage, 2n, PositionState.EMPTY)
-      expect(persisted).to.deep.equal([
+      expect(persisted).toEqual([
         await tree.getNode([0, 0]),
         await tree.getNode([0]),
         await tree.getNode([]),
@@ -43,25 +43,19 @@ describe(MerkleTree.name, () => {
     it('returns the node at a specified index', async () => {
       const storage = new InMemoryMerkleStorage()
       const tree = await MerkleTree.create(storage, 3n, PositionState.EMPTY)
-      expect(await tree.get(2n)).to.equal(PositionState.EMPTY)
+      expect(await tree.get(2n)).toEqual(PositionState.EMPTY)
     })
 
     it('throws for negative indices', async () => {
       const storage = new InMemoryMerkleStorage()
       const tree = await MerkleTree.create(storage, 3n, PositionState.EMPTY)
-      await expect(tree.get(-1n)).to.be.rejectedWith(
-        TypeError,
-        'Index out of bounds'
-      )
+      await expect(tree.get(-1n)).toBeRejected(TypeError, 'Index out of bounds')
     })
 
     it('throws for too large indices', async () => {
       const storage = new InMemoryMerkleStorage()
       const tree = await MerkleTree.create(storage, 3n, PositionState.EMPTY)
-      await expect(tree.get(8n)).to.be.rejectedWith(
-        TypeError,
-        'Index out of bounds'
-      )
+      await expect(tree.get(8n)).toBeRejected(TypeError, 'Index out of bounds')
     })
   })
 
@@ -73,16 +67,16 @@ describe(MerkleTree.name, () => {
       const storage = new InMemoryMerkleStorage()
       const tree = await MerkleTree.create(storage, 3n, PositionState.EMPTY)
 
-      expect(await tree.get(2n)).to.equal(PositionState.EMPTY)
-      expect(await tree.get(7n)).to.equal(PositionState.EMPTY)
+      expect(await tree.get(2n)).toEqual(PositionState.EMPTY)
+      expect(await tree.get(7n)).toEqual(PositionState.EMPTY)
 
       await tree.update([
         { index: 2n, value: positionA },
         { index: 7n, value: positionB },
       ])
 
-      expect(await tree.get(2n)).to.equal(positionA)
-      expect(await tree.get(7n)).to.equal(positionB)
+      expect(await tree.get(2n)).toEqual(positionA)
+      expect(await tree.get(7n)).toEqual(positionB)
     })
 
     it('throws for negative indices', async () => {
@@ -94,7 +88,7 @@ describe(MerkleTree.name, () => {
           { index: -1n, value: positionA },
           { index: 7n, value: positionB },
         ])
-      ).to.be.rejectedWith(TypeError, 'Index out of bounds')
+      ).toBeRejected(TypeError, 'Index out of bounds')
     })
 
     it('throws for too large indices', async () => {
@@ -106,7 +100,7 @@ describe(MerkleTree.name, () => {
           { index: 2n, value: positionA },
           { index: 8n, value: positionB },
         ])
-      ).to.be.rejectedWith(TypeError, 'Index out of bounds')
+      ).toBeRejected(TypeError, 'Index out of bounds')
     })
 
     it('throws for too large indices', async () => {
@@ -118,19 +112,19 @@ describe(MerkleTree.name, () => {
           { index: 2n, value: positionA },
           { index: 8n, value: positionB },
         ])
-      ).to.be.rejectedWith(TypeError, 'Index out of bounds')
+      ).toBeRejected(TypeError, 'Index out of bounds')
     })
 
     it('uses last value for multiple updates at the same index', async () => {
       const storage = new InMemoryMerkleStorage()
       const tree = await MerkleTree.create(storage, 3n, PositionState.EMPTY)
 
-      expect(await tree.get(2n)).to.equal(PositionState.EMPTY)
+      expect(await tree.get(2n)).toEqual(PositionState.EMPTY)
       await tree.update([
         { index: 2n, value: positionA },
         { index: 2n, value: positionB },
       ])
-      expect(await tree.get(2n)).to.equal(positionB)
+      expect(await tree.get(2n)).toEqual(positionB)
     })
 
     it('empty update array leaves the hash unchanged', async () => {
@@ -140,7 +134,7 @@ describe(MerkleTree.name, () => {
       const hashBefore = await tree.hash()
       await tree.update([])
       const hashAfter = await tree.hash()
-      expect(hashBefore).to.equal(hashAfter)
+      expect(hashBefore).toEqual(hashAfter)
     })
 
     it('non-empty update changes the hash', async () => {
@@ -153,7 +147,7 @@ describe(MerkleTree.name, () => {
         { index: 7n, value: positionB },
       ])
       const hashAfter = await tree.hash()
-      expect(hashBefore).to.not.equal(hashAfter)
+      expect(hashBefore).not.toEqual(hashAfter)
     })
 
     it('updates that do not change values do not change the hash', async () => {
@@ -163,7 +157,7 @@ describe(MerkleTree.name, () => {
       const hashBefore = await tree.hash()
       await tree.update([{ index: 2n, value: PositionState.EMPTY }])
       const hashAfter = await tree.hash()
-      expect(hashBefore).to.equal(hashAfter)
+      expect(hashBefore).toEqual(hashAfter)
     })
 
     it('has a correct hash after updates', async () => {
@@ -183,7 +177,7 @@ describe(MerkleTree.name, () => {
         { index: 7n, value: positionB },
       ])
 
-      expect(await tree.hash()).to.equal(
+      expect(await tree.hash()).toEqual(
         PedersenHash(
           '07b56631b126137c5e21afb5b9bc187d3cce0c549dfbbf58abdd6b00a3b3527a'
         )
@@ -204,7 +198,7 @@ describe(MerkleTree.name, () => {
         { index: 7n, value: positionB },
       ])
 
-      expect(persisted).to.deep.equal([
+      expect(persisted).toEqual([
         await tree.getNode([0, 1, 0]),
         await tree.getNode([0, 1]),
         await tree.getNode([0]),
@@ -229,7 +223,7 @@ describe(MerkleTree.name, () => {
       ])
 
       const recovered = new MerkleTree(storage, 3n, await tree.hash())
-      expect(await recovered.get(2n)).to.equal(positionA)
+      expect(await recovered.get(2n)).toEqual(positionA)
     })
 
     it('set hash changes the tree', async () => {
@@ -242,9 +236,9 @@ describe(MerkleTree.name, () => {
         { index: 7n, value: positionB },
       ])
 
-      expect(await treeB.get(2n)).to.equal(PositionState.EMPTY)
+      expect(await treeB.get(2n)).toEqual(PositionState.EMPTY)
       treeB.setHash(await treeA.hash())
-      expect(await treeB.get(2n)).to.equal(positionA)
+      expect(await treeB.get(2n)).toEqual(positionA)
     })
   })
 })
