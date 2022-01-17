97b4dd07c6348be5bcbc5f0f250b239d8dd51bf1
hasparus <hasparus@gmail.com>
2022-01-17 09:10:21 +0100

    Setup new database schema for every test suite

--- packages/backend/test/peripherals/database/DatabaseService.test.ts
+++ packages/backend/test/peripherals/database/DatabaseService.test.ts
@@ -1,11 +1,25 @@
 import { expect } from 'earljs'
+import { Knex } from 'knex'
 
+import { getConfig } from '../../../src/config'
+import { __SKIP_DB_TESTS__ } from '../../../src/config/config.testing'
 import { DatabaseService } from '../../../src/peripherals/database/DatabaseService'
 import { Logger } from '../../../src/tools/Logger'
-import { setupDatabaseTestSuite } from './setup'
 
 describe('DatabaseService', () => {
-  const { knex } = setupDatabaseTestSuite()
+  const config = getConfig('test')
+  let knex: Knex
+  const skip = config.databaseUrl === __SKIP_DB_TESTS__
+
+  before(async function () {
+    if (skip) {
+      this.skip()
+    }
+
+    knex = DatabaseService.createKnexInstance(config.databaseUrl)
+    await knex.schema.createSchema('test_DatabaseService')
+    await knex.raw("SET SCHEMA 'test_DatabaseService'")
+  })
 
   it('can run and rollback all migrations', async () => {
     const databaseService = new DatabaseService(knex, Logger.SILENT)
@@ -17,6 +31,13 @@ describe('DatabaseService', () => {
     )
     const tables = result.rows.map((x: { table_name: string }) => x.table_name)
 
-    expect(tables).toEqual(['knex_migrations', 'knex_migrations_lock'])
+    expect(tables).toEqual(
+      expect.arrayWith('knex_migrations', 'knex_migrations_lock')
+    )
+  })
+
+  after(async function () {
+    await knex.schema.dropSchema('test_DatabaseService', true)
+    await knex.destroy()
   })
 })
--- packages/backend/test/peripherals/database/setup.ts
+++ packages/backend/test/peripherals/database/setup.ts
@@ -1,4 +1,4 @@
-import type { Knex } from 'knex'
+import { randomUUID as uuid } from 'crypto'
 
 import { getConfig } from '../../../src/config'
 import { __SKIP_DB_TESTS__ } from '../../../src/config/config.testing'
@@ -8,18 +8,27 @@ export function setupDatabaseTestSuite() {
   const config = getConfig('test')
   const knex = DatabaseService.createKnexInstance(config.databaseUrl)
   const skip = config.databaseUrl === __SKIP_DB_TESTS__
+  let schemaName = ''
 
   before(async function () {
     if (skip) {
       this.skip()
     } else {
-      try {
-        await knex.migrate.latest()
-      } catch (err) {
-        console.error('database test suite setup failed')
-        await printTables(knex)
-        throw err
-      }
+      // For describe("one", () => describe("two") => {}) test suite we set up
+      // 'test_one_two' schema before running tests.
+
+      const titlePath = this.test?.parent?.titlePath()
+      schemaName = `test_${titlePath?.join('_') || uuid()}`
+
+      console.log('Creating test schema', schemaName)
+
+      // We drop before instead of after tests, so we can inspect the
+      // contents of database when tests fail.
+      await knex.raw(`DROP SCHEMA IF EXISTS "${schemaName}" CASCADE`)
+      await knex.raw(`CREATE SCHEMA "${schemaName}"`)
+      await knex.raw(`SET SCHEMA '${schemaName}'`)
+
+      await knex.migrate.latest({ schemaName })
     }
   })
 
@@ -28,11 +37,4 @@ export function setupDatabaseTestSuite() {
   })
 
   return { knex }
-}
-
-async function printTables(knex: Knex) {
-  const tables = await knex.raw(
-    'SELECT table_name, current_schema(), current_database() FROM information_schema.tables WHERE table_schema = current_schema()'
-  )
-  console.log('tables:', tables.rows)
 }
