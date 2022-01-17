7c6d41a6010b8fca4026daeddd34154f6ec4c60c
hasparus <hasparus@gmail.com>
2022-01-17 09:10:21 +0100

    Setup new database schema for every test suite

--- packages/backend/test/peripherals/database/setup.ts
+++ packages/backend/test/peripherals/database/setup.ts
@@ -1,3 +1,5 @@
+import { randomUUID as uuid } from 'crypto'
+
 import { getConfig } from '../../../src/config'
 import { __SKIP_DB_TESTS__ } from '../../../src/config/config.testing'
 import { DatabaseService } from '../../../src/peripherals/database/DatabaseService'
@@ -6,12 +8,27 @@ export function setupDatabaseTestSuite() {
   const config = getConfig('test')
   const knex = DatabaseService.createKnexInstance(config.databaseUrl)
   const skip = config.databaseUrl === __SKIP_DB_TESTS__
+  let schemaName = ''
 
   before(async function () {
     if (skip) {
       this.skip()
     } else {
-      await knex.migrate.latest()
+      // For describe("one", () => describe("two") => {}) test suite we set up
+      // 'test_one_two' schema before running tests.
+
+      const titlePath = this.test?.parent?.titlePath()
+      schemaName = `test_${titlePath?.join('_') || uuid()}`
+
+      console.log('Creating test schema', schemaName)
+
+      // We drop before instead of after tests, so we can inspect the
+      // contents of database when tests fail.
+      await knex.raw(`DROP SCHEMA IF EXISTS "${schemaName}" CASCADE`)
+      await knex.raw(`CREATE SCHEMA "${schemaName}"`)
+      await knex.raw(`SET SCHEMA '${schemaName}'`)
+
+      await knex.migrate.latest({ schemaName })
     }
   })
 
