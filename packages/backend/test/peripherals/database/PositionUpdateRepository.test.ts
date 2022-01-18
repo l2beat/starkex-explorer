31b543e60919aba2cb5e1f658b1e1affbb233792
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-01-18 14:39:00 +0100

    Fix non-deterministic tests

--- packages/backend/test/peripherals/database/PositionUpdateRepository.test.ts
+++ packages/backend/test/peripherals/database/PositionUpdateRepository.test.ts
@@ -72,7 +72,7 @@ describe(PositionUpdateRepository.name, () => {
 
     await repository.addOrUpdate(records)
     const actual = await repository.getAll()
-    expect(actual[0]).toEqual(records[0])
+    expect(actual).toBeAnArrayWith(...records)
   })
 
   it('deletes all records', async () => {
