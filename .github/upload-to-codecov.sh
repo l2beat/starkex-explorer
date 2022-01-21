06dc4d29894f37966d184898426043e228e5bf88
hasparus <hasparus@gmail.com>
2022-01-21 18:33:47 +0100

    Try Codecov path fixes

--- .github/upload-to-codecov.sh
+++ .github/upload-to-codecov.sh
@@ -14,7 +14,6 @@ for dir in */
       then
         file="$PWD/$package/coverage/lcov.info"
         if test -f "$file"; then
-          cd $package
           flag="${package/-/_}"
           $codecov_file -f $file -F $flag -v -t $CODECOV_TOKEN
         fi
