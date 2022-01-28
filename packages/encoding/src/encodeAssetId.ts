561da4c7bf032e11c2951e9226b9faaa6750798d
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-01-28 11:03:57 +0100

    Remove unnecessary check in encodeAssetId

--- packages/encoding/src/encodeAssetId.ts
+++ packages/encoding/src/encodeAssetId.ts
@@ -1,12 +1,9 @@
 import { AssetId } from './AssetId'
 
 export function encodeAssetId(assetId: AssetId) {
-  const bytes = assetId.split('').map((x) => x.charCodeAt(0))
-  if (bytes.some((x) => x > 255)) {
-    throw new Error('AssetId contains invalid characters')
-  }
-  return bytes
-    .map((x) => x.toString(16).padStart(2, '0'))
+  return assetId
+    .split('')
+    .map((x) => x.charCodeAt(0).toString(16).padStart(2, '0'))
     .join('')
     .padEnd(30, '0')
 }
