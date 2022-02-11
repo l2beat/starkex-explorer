e1bf3868aad999766557c1157333f9cb84de1659
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-02-11 14:59:21 +0100

    Account for dependencies when cache busting

--- packages/frontend/gulpfile.js
+++ packages/frontend/gulpfile.js
@@ -7,7 +7,7 @@ const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const fs = require('fs')
-const crypto = require('crypto')
+const { addHashes } = require('./scripts/hashStaticFiles')
 
 async function clean() {
   await del('build')
@@ -49,18 +49,11 @@ function buildPages() {
 }
 
 async function hashStaticFiles() {
-  const manifest = await addHashes('build/static')
-  await fs.promises.writeFile(
-    'build/manifest.json',
-    JSON.stringify(manifest, null, 2)
-  )
-}
-
-async function makeEmptyManifest() {
-  await fs.promises.writeFile(
-    'build/manifest.json',
-    JSON.stringify({}, null, 2)
-  )
+  return addHashes({
+    buildDir: 'build',
+    staticDir: 'build/static',
+    staticIgnoreList: ['/favicon.ico'],
+  })
 }
 
 function startPreview() {
@@ -71,14 +64,12 @@ function startPreview() {
 
 const build = gulp.series(
   clean,
-  makeEmptyManifest,
   gulp.parallel(buildScripts, buildStyles, buildPages, copyStatic),
   hashStaticFiles
 )
 
 const watch = gulp.series(
   clean,
-  makeEmptyManifest,
   gulp.parallel(buildScripts, buildStyles, copyStatic),
   gulp.parallel(watchScripts, watchStyles, watchStatic, startPreview)
 )
@@ -90,38 +81,6 @@ module.exports = {
 }
 
 // Utilities
-
-async function addHashes(dir, base = '/') {
-  const items = await fs.promises.readdir(dir)
-  let manifest = {}
-  for (const item of items) {
-    const fullName = path.join(dir, item)
-    if (fs.statSync(fullName).isDirectory()) {
-      const updates = await addHashes(fullName, path.join(base, item))
-      manifest = { ...manifest, ...updates }
-    } else {
-      const hash = (await sha256(fullName)).slice(0, 8)
-      const { name, ext } = path.parse(item)
-      const itemWithHash = `${name}.${hash}${ext}`
-      await fs.promises.rename(fullName, path.join(dir, itemWithHash))
-      manifest[path.join(base, item)] = path.join(base, itemWithHash)
-    }
-  }
-  return manifest
-}
-
-async function sha256(file) {
-  const fd = fs.createReadStream(file)
-  const hash = crypto.createHash('sha256')
-  hash.setEncoding('hex')
-  return new Promise((resolve) => {
-    fd.on('end', function () {
-      hash.end()
-      resolve(hash.read())
-    })
-    fd.pipe(hash)
-  })
-}
 
 function exec(command) {
   const nodeModulesHere = path.join(__dirname, './node_modules/.bin')
