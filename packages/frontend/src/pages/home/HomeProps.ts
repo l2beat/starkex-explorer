736e787fd1c33d6322b2cf12398b746c45186cda
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-02-14 11:28:46 +0100

    Add a basic home page

---
+++ packages/frontend/src/pages/home/HomeProps.ts
@@ -0,0 +1,19 @@
+import { PedersenHash } from '@explorer/crypto'
+
+export interface HomeProps {
+  stateUpdates: HomeStateUpdate[]
+  forcedTransaction: HomeForcedTransaction[]
+}
+
+export interface HomeStateUpdate {
+  hash: PedersenHash
+  timestamp: number
+  positionCount: number
+}
+
+export interface HomeForcedTransaction {
+  hash: string // TODO: Hash256
+  type: 'exit' | 'trade'
+  timestamp: number
+  valueUSDCents: number
+}
--- packages/frontend/src/preview/index.ts
+++ packages/frontend/src/preview/index.ts
@@ -4,12 +4,13 @@ import Koa from 'koa'
 import serve from 'koa-static'
 
 import { renderHomePage } from '../pages'
+import * as DATA from './data'
 
 const app = new Koa()
 const router = new Router()
 
 router.get('/', (ctx) => {
-  ctx.body = renderHomePage()
+  ctx.body = renderHomePage(DATA.HOME_PROPS)
 })
 
 app.use(router.routes())
