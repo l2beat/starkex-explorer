e1f0f087e855e2757253d2778e4e8ff1f78a7664
sin6pi7 <jacek@diffchecker.com>
2022-02-21 17:42:14 +0100

    WIP: add happy path rendering tests for frontend routes

--- packages/backend/test/api/routers/FrontendRouter.test.ts
+++ packages/backend/test/api/routers/FrontendRouter.test.ts
@@ -1,23 +1,67 @@
 import { FrontendController } from '../../../src/api/controllers/FrontendController'
 import { createFrontendRouter } from '../../../src/api/routers/FrontendRouter'
-import { StateUpdateRepository } from '../../../src/peripherals/database/StateUpdateRepository'
 import { mock } from '../../mock'
 import { createTestApiServer } from '../TestApiServer'
 
 describe('FrontendRouter', () => {
-  it('/ returns html', async () => {
-    const stateUpdateRepository = mock<StateUpdateRepository>({
-      getStateChangeList: async () => [],
+  describe('/', () => {
+    it('returns html', async () => {
+      const frontendRouter = createFrontendRouter(
+        mock<FrontendController>({
+          getHomePage: async () => '<!DOCTYPE html>',
+        }),
+      )
+      const server = createTestApiServer([frontendRouter])
+  
+      await server
+        .get('/')
+        .expect(200)
+        .expect(/^<!DOCTYPE html>/)
+    }) 
+  })
+  describe('/state-updates', () => {
+    it('returns html', async () => {
+      const frontendRouter = createFrontendRouter(
+        mock<FrontendController>({
+          getStateChangesPage: async () => '<!DOCTYPE html>',
+        }),
+      )
+      const server = createTestApiServer([frontendRouter])
+  
+      await server
+        .get('/state-updates')
+        .expect(200)
+        .expect(/^<!DOCTYPE html>/)
     })
-    const frontendController = new FrontendController(stateUpdateRepository)
-    const frontendRouter = createFrontendRouter(
-      frontendController,
-    )
-    const server = createTestApiServer([frontendRouter])
-
-    await server
-      .get('/')
-      .expect(200)
-      .expect(/^<!DOCTYPE html>/)
+  })
+  describe('/state-updates/:hash', () => {
+    it('returns html', async () => {
+      const frontendRouter = createFrontendRouter(
+        mock<FrontendController>({
+          getStateChangeDetailsPage: async () => '<!DOCTYPE html>',
+        }),
+      )
+      const server = createTestApiServer([frontendRouter])
+  
+      await server
+        .get('/state-updates/52ddcbdd431a044cf838a71d194248640210b316d7b1a568997ecad9dec9626')
+        .expect(200)
+        .expect(/^<!DOCTYPE html>/)
+    })
+  })
+  describe('/positions/:positionId', () => {
+    it('returns html', async () => {
+      const frontendRouter = createFrontendRouter(
+        mock<FrontendController>({
+          getPositionDetailsPage: async () => '<!DOCTYPE html>',
+        }),
+      )
+      const server = createTestApiServer([frontendRouter])
+  
+      await server
+        .get('/positions/1')
+        .expect(200)
+        .expect(/^<!DOCTYPE html>/)
+    })
   })
 })
