396d0c4bbf7a6a37dcebd1508493013662792210
sin6pi7 <jac.zyla@gmail.com>
2022-02-25 16:53:53 +0100

    Test StateUpdateCollector

--- packages/backend/src/core/StateUpdateCollector.ts
+++ packages/backend/src/core/StateUpdateCollector.ts
@@ -1,7 +1,8 @@
 import { PedersenHash } from '@explorer/crypto'
 import { decodeOnChainData as _decodeOnChainData } from '@explorer/encoding'
-import { Position, RollupState } from '@explorer/state'
+import { RollupState } from '@explorer/state'
 
+import { Hash256 } from '../model'
 import { PageRepository } from '../peripherals/database/PageRepository'
 import { RollupStateRepository } from '../peripherals/database/RollupStateRepository'
 import { StateTransitionFactRecord } from '../peripherals/database/StateTransitionFactsRepository'
@@ -20,6 +21,12 @@ export const ROLLUP_STATE_EMPTY_HASH = PedersenHash(
   '52ddcbdd431a044cf838a71d194248640210b316d7b1a568997ecad9dec9626'
 )
 
+interface StateTransition {
+  blockNumber: number
+  factHash: Hash256
+  pages: string[]
+}
+
 export class StateUpdateCollector {
   private rollupState?: RollupState
 
@@ -36,47 +43,55 @@ export class StateUpdateCollector {
       return
     }
 
-    const stateTransitions = await this.pageRepository.getAllForFacts(
+    const dbTransitions = await this.pageRepository.getAllForFacts(
       stateTransitionFacts.map((f) => f.hash)
     )
+    const stateTransitions = dbTransitions.map((x, i) => ({
+      ...x,
+      blockNumber: stateTransitionFacts[i].blockNumber,
+    }))
 
-    let { oldHash, id } = await this.readLastUpdate()
-    let rollupState = await this.ensureRollupState(oldHash)
+    const { oldHash, id } = await this.readLastUpdate()
+    await this.ensureRollupState(oldHash)
 
-    let i = 0
-    for (const { factHash, pages } of stateTransitions) {
-      const blockNumber = stateTransitionFacts[i].blockNumber
-
-      const { timestamp } = await this.ethereumClient.getBlock(blockNumber)
-
-      const decoded = this.decodeOnChainData(pages)
-
-      let newPositions: { index: bigint; value: Position }[]
-      ;[rollupState, newPositions] = await rollupState.update(decoded)
-      this.rollupState = rollupState
-
-      const rootHash = await rollupState.positions.hash()
-      await this.stateUpdateRepository.add({
-        stateUpdate: {
-          id: ++id,
-          blockNumber,
-          factHash,
-          rootHash,
-          timestamp,
-        },
-        positions: newPositions.map(
-          ({ value, index }): PositionRecord => ({
-            positionId: index,
-            publicKey: value.publicKey,
-            balances: value.assets,
-            collateralBalance: value.collateralBalance,
-          })
-        ),
-        prices: decoded.newState.oraclePrices,
-      })
-
-      i++
+    for (const [i, stateTransition] of stateTransitions.entries()) {
+      await this.processStateTransition(stateTransition, id + i + 1)
     }
+  }
+
+  async processStateTransition(
+    { pages, factHash, blockNumber }: StateTransition,
+    id: number
+  ) {
+    if (!this.rollupState) {
+      return
+    }
+    const { timestamp } = await this.ethereumClient.getBlock(blockNumber)
+
+    const decoded = this.decodeOnChainData(pages)
+
+    const [rollupState, newPositions] = await this.rollupState.update(decoded)
+    this.rollupState = rollupState
+
+    const rootHash = await rollupState.positions.hash()
+    await this.stateUpdateRepository.add({
+      stateUpdate: {
+        id,
+        blockNumber,
+        factHash,
+        rootHash,
+        timestamp,
+      },
+      positions: newPositions.map(
+        ({ value, index }): PositionRecord => ({
+          positionId: index,
+          publicKey: value.publicKey,
+          balances: value.assets,
+          collateralBalance: value.collateralBalance,
+        })
+      ),
+      prices: decoded.newState.oraclePrices,
+    })
   }
 
   async discardAfter(blockNumber: BlockNumber) {
@@ -84,19 +99,11 @@ export class StateUpdateCollector {
   }
 
   private async readLastUpdate() {
-    let oldHash: PedersenHash
-    let id: number
-
     const lastUpdate = await this.stateUpdateRepository.getLast()
-
     if (lastUpdate) {
-      ;({ rootHash: oldHash, id } = lastUpdate)
-    } else {
-      oldHash = ROLLUP_STATE_EMPTY_HASH
-      id = 0
+      return { oldHash: lastUpdate.rootHash, id: lastUpdate.id }
     }
-
-    return { oldHash, id }
+    return { oldHash: ROLLUP_STATE_EMPTY_HASH, id: 0 }
   }
 
   async ensureRollupState(oldHash: PedersenHash) {
--- packages/backend/test/core/StateUpdateCollector.test.ts
+++ packages/backend/test/core/StateUpdateCollector.test.ts
@@ -1,8 +1,5 @@
 import { PedersenHash } from '@explorer/crypto'
-import { AssetId, OnChainData } from '@explorer/encoding'
-import { Position } from '@explorer/state'
-import { expect } from 'earljs'
-import type { providers } from 'ethers'
+import { expect, mockFn } from 'earljs'
 
 import {
   ROLLUP_STATE_EMPTY_HASH,
@@ -11,24 +8,125 @@ import {
 import { Hash256 } from '../../src/model'
 import type { PageRepository } from '../../src/peripherals/database/PageRepository'
 import type { RollupStateRepository } from '../../src/peripherals/database/RollupStateRepository'
-import type { StateTransitionFactRecord } from '../../src/peripherals/database/StateTransitionFactsRepository'
-import type {
-  StateUpdateRecord,
-  StateUpdateRepository,
-} from '../../src/peripherals/database/StateUpdateRepository'
+import type { StateUpdateRepository } from '../../src/peripherals/database/StateUpdateRepository'
 import type { EthereumClient } from '../../src/peripherals/ethereum/EthereumClient'
 import { mock } from '../mock'
 
 describe(StateUpdateCollector.name, () => {
   describe(StateUpdateCollector.prototype.ensureRollupState.name, () => {
-    it('sets state for an initial update', async () => {})
-    it('leaves state a subsequent update')
-    it('sets state for the first update after restart')
-    it('resests state for the update after reorg')
+    it('sets state before an initial state transition', async () => {
+      const rollupStateRepository = mock<RollupStateRepository>({
+        persist: async () => {},
+      })
+      const stateUpdateCollector = new StateUpdateCollector(
+        mock<PageRepository>(),
+        mock<StateUpdateRepository>(),
+        rollupStateRepository,
+        mock<EthereumClient>()
+      )
+      const rollupState = await stateUpdateCollector.ensureRollupState(
+        ROLLUP_STATE_EMPTY_HASH
+      )
+      expect(await rollupState.positions.hash()).toEqual(
+        ROLLUP_STATE_EMPTY_HASH
+      )
+      expect(rollupStateRepository.persist.calls.length).toEqual(1)
+    })
+
+    it('sets state after restart', async () => {
+      const stateUpdateCollector = new StateUpdateCollector(
+        mock<PageRepository>(),
+        mock<StateUpdateRepository>(),
+        mock<RollupStateRepository>(),
+        mock<EthereumClient>()
+      )
+      const hash = PedersenHash.fake()
+      const rollupState = await stateUpdateCollector.ensureRollupState(hash)
+      expect(await rollupState.positions.hash()).toEqual(hash)
+    })
+
+    it('resests state after reorg', async () => {
+      const stateUpdateCollector = new StateUpdateCollector(
+        mock<PageRepository>(),
+        mock<StateUpdateRepository>(),
+        mock<RollupStateRepository>(),
+        mock<EthereumClient>()
+      )
+      const hashA = PedersenHash.fake('a')
+      const hashB = PedersenHash.fake('b')
+      const rollupStateA = await stateUpdateCollector.ensureRollupState(hashA)
+      const rollupStateB = await stateUpdateCollector.ensureRollupState(hashB)
+      expect(await rollupStateA.positions.hash()).toEqual(hashA)
+      expect(await rollupStateB.positions.hash()).toEqual(hashB)
+    })
+
+    it('leaves state intact before a subsequent state transition', async () => {
+      const stateUpdateCollector = new StateUpdateCollector(
+        mock<PageRepository>(),
+        mock<StateUpdateRepository>(),
+        mock<RollupStateRepository>(),
+        mock<EthereumClient>()
+      )
+      const hash = PedersenHash.fake()
+      const rollupStateA = await stateUpdateCollector.ensureRollupState(hash)
+      const rollupStateB = await stateUpdateCollector.ensureRollupState(hash)
+      expect(rollupStateA).toReferentiallyEqual(rollupStateB)
+    })
   })
 
   describe(StateUpdateCollector.prototype.save.name, () => {
-    it('calls processStateTransition for every update')
+    it('calls processStateTransition for every update', async () => {
+      const pageRepository = mock<PageRepository>({
+        getAllForFacts: async () => [
+          { factHash: Hash256.fake('a'), pages: ['aa', 'ab', 'ac'] },
+          { factHash: Hash256.fake('b'), pages: ['ba', 'bb'] },
+        ],
+      })
+      const stateUpdateRepository = mock<StateUpdateRepository>({
+        getLast: async () => ({
+          rootHash: PedersenHash.fake('1234'),
+          id: 567,
+          timestamp: Math.random(),
+          blockNumber: Math.random(),
+          factHash: Hash256.fake(),
+        }),
+      })
+      const stateUpdateCollector = new StateUpdateCollector(
+        pageRepository,
+        stateUpdateRepository,
+        mock<RollupStateRepository>(),
+        mock<EthereumClient>()
+      )
+      const processStateTransition = mockFn().resolvesTo(undefined)
+      stateUpdateCollector.processStateTransition = processStateTransition
+
+      await stateUpdateCollector.save([
+        { blockNumber: 123, hash: Hash256.fake('123') },
+        { blockNumber: 456, hash: Hash256.fake('456') },
+      ])
+      expect(processStateTransition).toHaveBeenCalledExactlyWith([
+        [
+          {
+            blockNumber: 123,
+            factHash: Hash256.fake('a'),
+            pages: ['aa', 'ab', 'ac'],
+          },
+          567 + 1,
+        ],
+        [
+          {
+            blockNumber: 456,
+            factHash: Hash256.fake('b'),
+            pages: ['ba', 'bb'],
+          },
+          567 + 2,
+        ],
+      ])
+    })
+  })
+
+  describe(StateUpdateCollector.prototype.processStateTransition.name, () => {
+    it('saves the state update to the database')
   })
 
   describe(StateUpdateCollector.prototype.discardAfter.name, () => {
