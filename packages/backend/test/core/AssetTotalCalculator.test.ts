4096a886b2881ee21c1bda94e998829a4fb5c232
sin6pi7 <jac.zyla@gmail.com>
2022-03-04 13:39:30 +0100

    Update calculator tests

--- packages/backend/test/core/AssetTotalCalculator.test.ts
+++ packages/backend/test/core/AssetTotalCalculator.test.ts
@@ -4,22 +4,23 @@ import { expect } from 'earljs'
 import { assetTotalUSDCents } from '../../src/core/AssetTotalCalculator'
 
 describe(assetTotalUSDCents.name, () => {
-  it('caculates value properly', () => {
-    const cases = [
-      {
-        balance: 1000n,
-        price: 22042658710n,
-        assetId: 'BTC-10',
-        total: 51322064152n,
-      },
-      {
-        balance: 1000n,
-        price: 7611149047n,
-        assetId: 'ETH-9',
-        total: 1772108731n,
-      },
-    ]
-    cases.forEach(({ balance, price, assetId, total }) => {
+  const cases = [
+    {
+      balance: 1000n,
+      price: 22042658710n,
+      assetId: 'BTC-10',
+      total: 51322064152n,
+    },
+    {
+      balance: 1000n,
+      price: 7611149047n,
+      assetId: 'ETH-9',
+      total: 1772108731n,
+    },
+  ]
+
+  cases.forEach(({ balance, price, assetId, total }) => {
+    it('caculates value properly', () => {
       expect(assetTotalUSDCents(balance, price, AssetId(assetId))).toEqual(
         total
       )
