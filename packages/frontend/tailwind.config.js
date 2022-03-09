88f8ea8a0bc5dc53b130cd52a57942cbeb78eb72
sin6pi7 <jac.zyla@gmail.com>
2022-03-09 12:38:45 +0100

    Add colors to tailwind config

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -7,6 +7,22 @@ module.exports = {
         sans: ['Roboto', 'sans-serif'],
       },
     },
+    colors: {
+      grey: {
+        100: '#20252F',
+        200: '#2D2D3A',
+        300: '#374354',
+        400: '#838F9F',
+      },
+      white: '#838F9F',
+      blue: {
+        100: '#3067BB',
+        200: '#5196FF',
+      },
+      orange: {
+        100: '#9C4D03',
+      },
+    },
   },
   plugins: [],
 }
