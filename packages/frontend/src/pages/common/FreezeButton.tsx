4686a1144ce7004039a0726e2f6ac7d3f1a2c6fc
sin6pi7 <jac.zyla@gmail.com>
2022-03-17 10:56:29 +0100

    Design

---
+++ packages/frontend/src/pages/common/FreezeButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import cx from 'classnames'
+
+type FreezeButtonProps = {
+  active?: boolean
+}
+
+export function FreezeButton({ active }: FreezeButtonProps) {
+  const text = active ? 'Freeze' : 'Freeze (inactive)'
+  const activeClasses =
+    'px-4 text-white font-bold text-2xl leading-7 bg-blue-100'
+  const inactiveClasses = 'p-4 bg-grey-200 text-grey-400 leading-5'
+  return (
+    <button
+      className={cx(
+        'rounded-md w-[25%] h-[50px]',
+        active ? activeClasses : inactiveClasses
+      )}
+      disabled={!active}
+    >
+      {text}
+    </button>
+  )
+}
--- packages/frontend/src/pages/home/Home.tsx
+++ packages/frontend/src/pages/home/Home.tsx
@@ -6,6 +6,24 @@ import { formatTime } from '../formatTime'
 import { formatHash } from '../formatHash'
 import { Table } from '../common/Table'
 import { SimpleLink } from '../common/SimpleLink'
+import { FreezeButton } from '../common/FreezeButton'
+
+const stats = [
+  { title: 'Total Value Locked', value: '$5.24B' },
+  { title: 'State updates', value: '5143' },
+  { title: 'Tracked positions', value: '45,762' },
+]
+
+const Stat = ({ title, value }: { title: string; value: string }) => {
+  return (
+    <div className="py-2 px-4 bg-grey-300 rounded-md w-[25%]">
+      <div className="w-full mb-2">{title}</div>
+      <div className="font-sans font-bold text-2xl w-full text-right">
+        {value}
+      </div>
+    </div>
+  )
+}
 
 export function Home(props: HomeProps) {
   return (
@@ -18,6 +36,12 @@ export function Home(props: HomeProps) {
       scripts={['/scripts/main.js']}
       withoutSearch
     >
+      <div className="mb-12 flex gap-x-4 items-center">
+        {stats.map(({ title, value }) => (
+          <Stat key={title} title={title} value={value} />
+        ))}
+        <FreezeButton />
+      </div>
       <SearchBar className="drop-shadow-lg mb-12" />
       <div className="mb-1.5">
         <span className="float-left font-medium text-lg">
