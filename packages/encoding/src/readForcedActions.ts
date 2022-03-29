bf190af631eba297b389fc88dbe8f45e12d97303
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-03-29 19:04:56 +0200

    Add decoding information for remaining data

---
+++ packages/encoding/src/readForcedActions.ts
@@ -0,0 +1,45 @@
+import { ByteReader } from './ByteReader'
+import { decodeAssetId } from './decodeAssetId'
+import { DecodingError } from './DecodingError'
+import { ForcedAction } from './OnChainData'
+
+export function readForcedActions(reader: ByteReader) {
+  const count = reader.readNumber(32)
+  const forcedActions: ForcedAction[] = []
+  for (let i = 0; i < count; i++) {
+    const type = reader.readNumber(32)
+    if (type === 0) {
+      const publicKey = reader.readHex(32)
+      const positionId = reader.readBigInt(32)
+      const amount = reader.readBigInt(32)
+      forcedActions.push({ type: 'withdrawal', publicKey, positionId, amount })
+    } else if (type === 1) {
+      const publicKeyA = reader.readHex(32)
+      const publicKeyB = reader.readHex(32)
+      const positionIdA = reader.readBigInt(32)
+      const positionIdB = reader.readBigInt(32)
+      reader.skip(17)
+      const syntheticAssetId = decodeAssetId(reader.read(15))
+      const collateralAmount = reader.readBigInt(32)
+      const syntheticAmount = reader.readBigInt(32)
+      const isABuyingSynthetic = reader.readBigInt(32) != 0n
+      const nonce = reader.readBigInt(32)
+      forcedActions.push({
+        type: 'trade',
+        publicKeyA,
+        publicKeyB,
+        positionIdA,
+        positionIdB,
+        syntheticAssetId,
+        collateralAmount,
+        syntheticAmount,
+        isABuyingSynthetic,
+        nonce,
+      })
+    } else {
+      throw new DecodingError(`Invalid forced action type: ${type}`)
+    }
+  }
+  reader.skip(32) // data size, not sure what this is
+  return forcedActions
+}
---
+++ packages/encoding/src/readModifications.ts
@@ -0,0 +1,14 @@
+import { ByteReader } from './ByteReader'
+import { Modification } from './OnChainData'
+
+export function readModifications(reader: ByteReader) {
+  const count = reader.readNumber(32)
+  const modifications: Modification[] = []
+  for (let i = 0; i < count; i++) {
+    const publicKey = reader.readHex(32)
+    const positionId = reader.readBigInt(32)
+    const difference = reader.readBigInt(32) - 2n ** 64n
+    modifications.push({ publicKey, positionId, difference })
+  }
+  return modifications
+}
