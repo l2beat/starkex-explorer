96f4f5f2dc0d837db348070bd0869d279d9546c2
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-03-29 19:10:23 +0200

    Add tests for conditions and modifications

---
+++ packages/encoding/test/readConditions.test.ts
@@ -0,0 +1,38 @@
+import { expect } from 'earljs'
+
+import { DecodingError } from '../src'
+import { readConditions } from '../src/readConditions'
+import { ByteWriter } from './ByteWriter'
+import { readToDecode } from './readToDecode'
+
+describe(readConditions.name, () => {
+  const decode = readToDecode(readConditions)
+
+  it('fails for empty data', () => {
+    expect(() => decode('')).toThrow(DecodingError, 'Went out of bounds')
+  })
+
+  it('can read zero conditions', () => {
+    const writer = new ByteWriter().writeNumber(0, 32)
+    expect(decode(writer.getBytes())).toEqual([])
+  })
+
+  it('can read a single condition', () => {
+    const writer = new ByteWriter()
+      .writeNumber(1, 32)
+      .write('1234abcd'.repeat(8))
+    expect(decode(writer.getBytes())).toEqual(['0x' + '1234abcd'.repeat(8)])
+  })
+
+  it('can read multiple conditions', () => {
+    const writer = new ByteWriter()
+      .writeNumber(2, 32)
+      .write('1234abcd'.repeat(8))
+      .write('deadbeef'.repeat(8))
+
+    expect(decode(writer.getBytes())).toEqual([
+      '0x' + '1234abcd'.repeat(8),
+      '0x' + 'deadbeef'.repeat(8),
+    ])
+  })
+})
