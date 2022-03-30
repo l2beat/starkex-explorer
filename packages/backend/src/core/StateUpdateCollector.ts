86a05e8319bc43aeb3873053755a15847a6c477d
sin6pi7 <jac.zyla@gmail.com>
2022-03-30 14:31:44 +0200

    Add tests for mismatching state calculations

--- packages/backend/src/core/StateUpdateCollector.ts
+++ packages/backend/src/core/StateUpdateCollector.ts
@@ -1,4 +1,4 @@
-import { decodeOnChainData as _decodeOnChainData } from '@explorer/encoding'
+import { decodeOnChainData } from '@explorer/encoding'
 import { RollupState } from '@explorer/state'
 import { Hash256, PedersenHash } from '@explorer/types'
 
@@ -27,14 +27,12 @@ interface StateTransition {
 }
 
 export class StateUpdateCollector {
-  private rollupState?: RollupState
-
   constructor(
     private readonly pageRepository: PageRepository,
     private readonly stateUpdateRepository: StateUpdateRepository,
     private readonly rollupStateRepository: RollupStateRepository,
     private readonly ethereumClient: EthereumClient,
-    private readonly decodeOnChainData = _decodeOnChainData
+    private rollupState?: RollupState
   ) {}
 
   async save(stateTransitionFacts: StateTransitionFactRecord[]) {
@@ -70,7 +68,7 @@ export class StateUpdateCollector {
     }
     const { timestamp } = await this.ethereumClient.getBlock(blockNumber)
 
-    const decoded = this.decodeOnChainData(pages)
+    const decoded = decodeOnChainData(pages)
 
     const [rollupState, newPositions] = await this.rollupState.update(decoded)
     this.rollupState = rollupState
