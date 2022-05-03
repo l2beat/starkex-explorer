cbd088bd0c1b12d84f7490faa601a2fa472d4ad7
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-05-03 16:41:47 +0200

    More work on form

--- packages/frontend/src/pages/transaction-form/TransactionForm.tsx
+++ packages/frontend/src/pages/transaction-form/TransactionForm.tsx
@@ -1,6 +1,8 @@
+import { AssetId } from '@explorer/types'
 import React from 'react'
 
 import { Page } from '../common'
+import { AssetIcon } from '../common/icons/AssetIcon'
 import { TransactionFormProps } from './TransactionFormProps'
 
 export function TransactionForm(props: TransactionFormProps) {
@@ -33,8 +35,30 @@ export function TransactionForm(props: TransactionFormProps) {
           <input
             type="text"
             placeholder="0.00"
-            className="font-mono text-2xl leading-none bg-grey-100 rounded-md pl-4 pt-4 pr-[100px] pb-8 w-full"
+            className="font-mono text-2xl leading-none bg-grey-100 rounded-md pl-2 pt-2 pr-[100px] pb-8 w-full"
           />
+          <div className="absolute top-2 right-2 px-3 py-1 bg-grey-300 flex gap-2 items-center rounded-md">
+            <AssetIcon className="w-4 h-4" assetId={AssetId('USDC-6')} />
+            <span>USDC</span>
+            <svg
+              viewBox="0 0 10 5"
+              width="10"
+              height="5"
+              className="fill-white"
+            >
+              <path d="M0 0L10 0L5 5D" />
+            </svg>
+            <select className="absolute top-0 left-0 w-full h-full opacity-0">
+              {props.assets.map((asset) => (
+                <option
+                  key={asset.assetId.toString()}
+                  value={asset.assetId.toString()}
+                >
+                  {AssetId.symbol(asset.assetId)}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       </form>
     </Page>
