92ae519581a9e0ee9b279622f70521854e19ab2c
Michal <michalsidzej@gmail.com>
2022-05-11 11:31:03 +0100

    fix tests

--- packages/encoding/src/decodeAssetId.ts
+++ packages/encoding/src/decodeAssetId.ts
@@ -4,11 +4,12 @@ import { DYDX_INTERNAL_USDC_ID_ENDODED } from './constants'
 import { DecodingError } from './DecodingError'
 
 export function decodeAssetId(value: string) {
-  if (value.length !== 30) {
-    throw new DecodingError('Invalid AssetId length')
-  }
   if (value === DYDX_INTERNAL_USDC_ID_ENDODED) {
     return AssetId.USDC
+  }
+
+  if (value.length !== 30) {
+    throw new DecodingError('Invalid AssetId length')
   }
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const idString = value
