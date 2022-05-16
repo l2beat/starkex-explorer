fb6ce7e8608c97c6238d78ee3f7eeb4116410949
Jacek <jac.zyla@gmail.com>
2022-05-16 13:38:52 +0200

    Reorganise forced transactions (#125)
    * Add migration
    
    * First big part
    
    * Clean up
    
    * Simplify code
    
    * Update rest of the codebase to new model
    
    * Order in sql
    
    * Clean up
    
    * Make util functions more specific
    
    * Use BaseRepository for ForcedTransactions
    
    * Fix ForcedEventsCollector
    
    * Clean up test fakes
    
    * Fix fakes
    
    * Add tests for ForcedEventsCollector
    
    * Fix build
    
    * Fix migration
    
    * Clean up
    
    * Clean up migration
    
    * Use up/down from previous transactions
    
    * Fix imports

---
+++ packages/backend/src/peripherals/database/migrations/014_forced_transactions.ts
@@ -0,0 +1,39 @@
+/*
+                      ====== IMPORTANT NOTICE ======
+
+DO NOT EDIT OR RENAME THIS FILE
+
+This is a migration file. Once created the file should not be renamed or edited,
+because migrations are only run once on the production server. 
+
+If you find that something was incorrectly set up in the `up` function you
+should create a new migration file that fixes the issue.
+
+*/
+
+import { Knex } from 'knex'
+
+import {
+  down as dropForcedTransactionEvents,
+  up as createForcedTransactionEvents,
+} from './012_forced_transaction_events'
+
+export async function up(knex: Knex) {
+  await dropForcedTransactionEvents(knex)
+  await knex.schema.createTable('forced_transactions', (table) => {
+    table.string('hash').notNullable().index()
+    table.string('type').notNullable().index()
+    table.jsonb('data').notNullable()
+    table.string('data_hash').notNullable().index()
+    table
+      .integer('state_update_id')
+      .references('id')
+      .inTable('state_updates')
+      .onDelete('set null')
+  })
+}
+
+export async function down(knex: Knex) {
+  await knex.schema.dropTable('forced_transactions')
+  await createForcedTransactionEvents(knex)
+}
