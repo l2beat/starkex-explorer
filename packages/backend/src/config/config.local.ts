19e2273e9aa1a6682ae929fb3e8855a54f24d8a4
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2022-05-19 12:03:05 +0200

    Add contract addresses to config

--- packages/backend/src/config/config.local.ts
+++ packages/backend/src/config/config.local.ts
@@ -1,3 +1,4 @@
+import { EthereumAddress } from '@explorer/types'
 import { config as dotenv } from 'dotenv'
 
 import { LogLevel } from '../tools/Logger'
@@ -19,6 +20,15 @@ export function getLocalConfig(): Config {
     core: {
       syncBatchSize: getEnv.integer('SYNC_BATCH_SIZE', 6_000),
       maxBlockNumber: getEnv.integer('MAX_BLOCK_NUMBER', Infinity),
+    },
+    contracts: {
+      perpetual: EthereumAddress('0xD54f502e184B6B739d7D27a6410a67dc462D69c8'),
+      registry: EthereumAddress('0xEfbCcE4659db72eC6897F46783303708cf9ACef8'),
+      proxy: EthereumAddress('0xC8c212f11f6ACca77A7afeB7282dEBa5530eb46C'),
+      verifiers: [
+        EthereumAddress('0xB1EDA32c467569fbDC8C3E041C81825D76b32b84'),
+        EthereumAddress('0x894c4a12548FB18EaA48cF34f9Cd874Fc08b7FC3'),
+      ],
     },
     freshStart: getEnv.boolean('FRESH_START', false),
   }
--- packages/backend/src/config/config.production.ts
+++ packages/backend/src/config/config.production.ts
@@ -1,3 +1,5 @@
+import { EthereumAddress } from '@explorer/types'
+
 import { LogLevel } from '../tools/Logger'
 import { Config } from './Config'
 import { getEnv } from './getEnv'
@@ -19,6 +21,15 @@ export function getProductionConfig(): Config {
     core: {
       syncBatchSize: getEnv.integer('SYNC_BATCH_SIZE', 6_000),
       maxBlockNumber: getEnv.integer('MAX_BLOCK_NUMBER', Infinity),
+    },
+    contracts: {
+      perpetual: EthereumAddress('0xD54f502e184B6B739d7D27a6410a67dc462D69c8'),
+      registry: EthereumAddress('0xEfbCcE4659db72eC6897F46783303708cf9ACef8'),
+      proxy: EthereumAddress('0xC8c212f11f6ACca77A7afeB7282dEBa5530eb46C'),
+      verifiers: [
+        EthereumAddress('0xB1EDA32c467569fbDC8C3E041C81825D76b32b84'),
+        EthereumAddress('0x894c4a12548FB18EaA48cF34f9Cd874Fc08b7FC3'),
+      ],
     },
     freshStart: false,
   }
