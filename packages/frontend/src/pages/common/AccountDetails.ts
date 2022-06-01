c093867a1925eb5df65ffc0d1411f3327bd455ba
Michal <michalsidzej@gmail.com>
2022-06-01 20:28:31 +0200

    Fix zod bug

--- packages/frontend/src/pages/common/AccountDetails.ts
+++ packages/frontend/src/pages/common/AccountDetails.ts
@@ -1,10 +1,10 @@
-import { stringAs } from '@explorer/shared'
+import { stringAs, stringAsBigInt } from '@explorer/shared'
 import { EthereumAddress } from '@explorer/types'
 import { z } from 'zod'
 
 export type AccountDetails = z.infer<typeof AccountDetails>
 export const AccountDetails = z.object({
   address: stringAs(EthereumAddress),
-  positionId: z.bigint().optional(),
+  positionId: stringAsBigInt().optional(),
   hasUpdates: z.boolean(),
 })
