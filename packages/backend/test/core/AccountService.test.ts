e8a9c8d908f4575c251dca4b7f3a625b8dee8d16
Michal <michalsidzej@gmail.com>
2022-06-02 11:01:53 +0200

    Add unit test

--- packages/backend/test/core/AccountService.test.ts
+++ packages/backend/test/core/AccountService.test.ts
@@ -2,12 +2,16 @@ import { EthereumAddress } from '@explorer/types'
 import { expect } from 'earljs'
 
 import { AccountService } from '../../src/core/AccountService'
+import { ForcedTradeOfferRepository } from '../../src/peripherals/database/ForcedTradeOfferRepository'
 import { PositionRepository } from '../../src/peripherals/database/PositionRepository'
 import { mock } from '../mock'
 
 describe(AccountService.name, () => {
   it('returns undefined for undefined', async () => {
-    const accountService = new AccountService(mock<PositionRepository>())
+    const accountService = new AccountService(
+      mock<PositionRepository>(),
+      mock<ForcedTradeOfferRepository>()
+    )
     const result = await accountService.getAccount(undefined)
     expect(result).toEqual(undefined)
   })
@@ -16,14 +20,13 @@ describe(AccountService.name, () => {
     const accountService = new AccountService(
       mock<PositionRepository>({
         findIdByEthereumAddress: async () => undefined,
-      })
+      }),
+      mock<ForcedTradeOfferRepository>()
     )
     const address = EthereumAddress.fake()
     const result = await accountService.getAccount(address)
     expect(result).toEqual({
       address,
-      hasUpdates: false,
-      positionId: undefined,
     })
   })
 
@@ -31,6 +34,9 @@ describe(AccountService.name, () => {
     const accountService = new AccountService(
       mock<PositionRepository>({
         findIdByEthereumAddress: async () => 123n,
+      }),
+      mock<ForcedTradeOfferRepository>({
+        countPendingByPositionIdA: async () => 0,
       })
     )
     const address = EthereumAddress.fake()
@@ -38,6 +44,24 @@ describe(AccountService.name, () => {
     expect(result).toEqual({
       address,
       hasUpdates: false,
+      positionId: 123n,
+    })
+  })
+
+  it('returns has updates when pending offers exist', async () => {
+    const accountService = new AccountService(
+      mock<PositionRepository>({
+        findIdByEthereumAddress: async () => 123n,
+      }),
+      mock<ForcedTradeOfferRepository>({
+        countPendingByPositionIdA: async () => 1,
+      })
+    )
+    const address = EthereumAddress.fake()
+    const result = await accountService.getAccount(address)
+    expect(result).toEqual({
+      address,
+      hasUpdates: true,
       positionId: 123n,
     })
   })
