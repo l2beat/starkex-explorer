5e1be9ad068f8dfc9c462c33c93966274e86587f
Mateusz Kelner <mateuszkelner@gmail.com>
2022-12-13 11:31:41 +0100

    Run yarn format:fix

--- packages/encoding/test/decoding/decodeDexOutput.test.ts
+++ packages/encoding/test/decoding/decodeDexOutput.test.ts
@@ -1,15 +1,15 @@
-import { expect } from "earljs";
+import { expect } from 'earljs'
 
-import { decodeDexOutput } from "../../src";
+import { decodeDexOutput } from '../../src'
 import DECODED_EXAMPLE from '../data/spot-decoded-example.json'
 import ENCODED_EXAMPLE from '../data/spot-encoded-example.json'
 
 describe('decodeOnChainSpotData', () => {
-    it('decodes the example data', () => {
-        const decoded = decodeDexOutput(ENCODED_EXAMPLE)
-        const noBigInt = JSON.parse(
-        JSON.stringify(decoded, (k, v) => (typeof v === 'bigint' ? Number(v) : v))
-        )
-        expect(noBigInt).toEqual(DECODED_EXAMPLE)
-    })
+  it('decodes the example data', () => {
+    const decoded = decodeDexOutput(ENCODED_EXAMPLE)
+    const noBigInt = JSON.parse(
+      JSON.stringify(decoded, (k, v) => (typeof v === 'bigint' ? Number(v) : v))
+    )
+    expect(noBigInt).toEqual(DECODED_EXAMPLE)
+  })
 })
