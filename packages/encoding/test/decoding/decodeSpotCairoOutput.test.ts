a38a16a703169e9a0e65f78ef3a2a5194177c5d2
Mateusz Kelner <mateuszkelner@gmail.com>
2023-01-11 00:26:43 +0100

    Fix decodeSpotCairoOutput test

--- packages/encoding/test/decoding/decodeSpotCairoOutput.test.ts
+++ packages/encoding/test/decoding/decodeSpotCairoOutput.test.ts
@@ -5,8 +5,7 @@ import DECODED_EXAMPLE from '../data/spot-decoded-example.json'
 import ENCODED_EXAMPLE from '../data/spot-encoded-example.json'
 
 describe('decodeOnChainSpotData', () => {
-  // TODO: fix this test
-  xit('decodes the example data', () => {
+  it('decodes the example data', () => {
     const decoded = decodeSpotCairoOutput(ENCODED_EXAMPLE)
     const noBigInt = JSON.parse(
       JSON.stringify(decoded, (k, v) => (typeof v === 'bigint' ? Number(v) : v))
