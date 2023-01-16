8666d6f304aa5d34429d1b05a3e24acd7be34d45
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-01-16 17:39:46 +0100

    Handle possible transaction statuses

--- packages/backend/src/peripherals/database/ForcedWithdrawRepository.test.ts
+++ packages/backend/src/peripherals/database/ForcedWithdrawRepository.test.ts
@@ -14,15 +14,150 @@ describe(ForcedWithdrawRepository.name, () => {
 
   afterEach(() => repository.deleteAll())
 
-  it('adds single record and queries it', async () => {
+  describe('status history', () => {
     const record: ForcedWithdrawTransactionRecord = {
       hash: Hash256.fake(),
       starkKey: StarkKey.fake(),
       amount: 1n,
       positionId: 2n,
     }
-    await repository.addSent({ ...record, sentAt: Timestamp.now() })
-    const result = await repository.findByTransactionHash(record.hash)
-    expect(result).toEqual(record)
+
+    const sentTimestamp = Timestamp.now()
+    const forgottenTimestamp = Timestamp(Number(sentTimestamp) + 1000)
+    const revertedTimestamp = Timestamp(Number(forgottenTimestamp) + 1000)
+    const minedTimestamp = Timestamp(Number(revertedTimestamp) + 1000)
+    const includedTimestamp = Timestamp(Number(minedTimestamp) + 1000)
+
+    it('adds single record and queries it', async () => {
+      await repository.addSent({ ...record, timestamp: sentTimestamp })
+
+      const result = await repository.findByTransactionHash(record.hash)
+      expect(result).toEqual({
+        ...record,
+        history: [{ status: 'sent', timestamp: sentTimestamp }],
+      })
+    })
+
+    it('returns undefined for unknown hash', async () => {
+      const result = await repository.findByTransactionHash(record.hash)
+      expect(result).toEqual(undefined)
+    })
+
+    it('can track a regular flow', async () => {
+      await repository.addSent({ ...record, timestamp: sentTimestamp })
+      await repository.addMined({
+        ...record,
+        timestamp: minedTimestamp,
+        blockNumber: 123,
+      })
+      await repository.addIncluded({
+        hash: record.hash,
+        timestamp: includedTimestamp,
+        blockNumber: 456,
+        stateUpdateId: 100,
+      })
+
+      const result = await repository.findByTransactionHash(record.hash)
+      expect(result).toEqual({
+        ...record,
+        history: [
+          { status: 'sent', timestamp: sentTimestamp },
+          { status: 'mined', timestamp: minedTimestamp, blockNumber: 123 },
+          {
+            status: 'included',
+            timestamp: includedTimestamp,
+            blockNumber: 456,
+            stateUpdateId: 100,
+          },
+        ],
+      })
+    })
+
+    it('can track a forgotten flow', async () => {
+      await repository.addSent({ ...record, timestamp: sentTimestamp })
+      await repository.addForgotten({
+        hash: record.hash,
+        timestamp: forgottenTimestamp,
+      })
+
+      const result = await repository.findByTransactionHash(record.hash)
+      expect(result).toEqual({
+        ...record,
+        history: [
+          { status: 'sent', timestamp: sentTimestamp },
+          { status: 'forgotten', timestamp: forgottenTimestamp },
+        ],
+      })
+    })
+
+    it('can track a reverted flow', async () => {
+      await repository.addSent({ ...record, timestamp: sentTimestamp })
+      await repository.addReverted({
+        hash: record.hash,
+        timestamp: revertedTimestamp,
+        blockNumber: 123,
+      })
+
+      const result = await repository.findByTransactionHash(record.hash)
+      expect(result).toEqual({
+        ...record,
+        history: [
+          { status: 'sent', timestamp: sentTimestamp },
+          {
+            status: 'reverted',
+            timestamp: revertedTimestamp,
+            blockNumber: 123,
+          },
+        ],
+      })
+    })
+
+    it('orders events by timestamp', async () => {
+      await repository.addIncluded({
+        hash: record.hash,
+        timestamp: includedTimestamp,
+        blockNumber: 789,
+        stateUpdateId: 100,
+      })
+      await repository.addReverted({
+        hash: record.hash,
+        timestamp: revertedTimestamp,
+        blockNumber: 123,
+      })
+      await repository.addMined({
+        ...record,
+        hash: record.hash,
+        timestamp: minedTimestamp,
+        blockNumber: 456,
+      })
+      await repository.addSent({ ...record, timestamp: sentTimestamp })
+
+      const result = await repository.findByTransactionHash(record.hash)
+      expect(result).toEqual({
+        ...record,
+        history: [
+          { status: 'sent', timestamp: sentTimestamp },
+          {
+            status: 'reverted',
+            timestamp: revertedTimestamp,
+            blockNumber: 123,
+          },
+          { status: 'mined', timestamp: minedTimestamp, blockNumber: 456 },
+          {
+            status: 'included',
+            timestamp: includedTimestamp,
+            blockNumber: 789,
+            stateUpdateId: 100,
+          },
+        ],
+      })
+    })
+
+    it('prevents adding the same status twice', async () => {
+      await repository.addSent({ ...record, timestamp: sentTimestamp })
+      await expect(
+        repository.addSent({ ...record, timestamp: sentTimestamp })
+      ).toBeRejected()
+    })
   })
 })
--- packages/backend/src/peripherals/database/ForcedWithdrawRepository.ts
+++ packages/backend/src/peripherals/database/ForcedWithdrawRepository.ts
@@ -1,5 +1,8 @@
 import { Hash256, StarkKey, Timestamp } from '@explorer/types'
-import { ForcedWithdrawTransactionRow } from 'knex/types/tables'
+import {
+  ForcedWithdrawStatusRow,
+  ForcedWithdrawTransactionRow,
+} from 'knex/types/tables'
 
 import { Logger } from '../../tools/Logger'
 import { BaseRepository } from './shared/BaseRepository'
@@ -12,6 +15,47 @@ export interface ForcedWithdrawTransactionRecord {
   positionId: bigint
 }
 
+export interface ForcedWithdrawTransactionWithHistory
+  extends ForcedWithdrawTransactionRecord {
+  history: ForcedWithdrawStatus[]
+}
+
+export type ForcedWithdrawStatus =
+  | SentStatus
+  | MinedStatus
+  | RevertedStatus
+  | ForgottenStatus
+  | IncludedStatus
+
+export interface SentStatus {
+  status: 'sent'
+  timestamp: Timestamp
+}
+
+export interface MinedStatus {
+  status: 'mined'
+  timestamp: Timestamp
+  blockNumber: number
+}
+
+export interface RevertedStatus {
+  status: 'reverted'
+  timestamp: Timestamp
+  blockNumber: number
+}
+
+export interface ForgottenStatus {
+  status: 'forgotten'
+  timestamp: Timestamp
+}
+
+export interface IncludedStatus {
+  status: 'included'
+  timestamp: Timestamp
+  blockNumber: number
+  stateUpdateId: number
+}
+
 export class ForcedWithdrawRepository extends BaseRepository {
   constructor(database: Database, logger: Logger) {
     super(database, logger)
@@ -19,16 +63,17 @@ export class ForcedWithdrawRepository extends BaseRepository {
     /* eslint-disable @typescript-eslint/unbound-method */
 
     this.addSent = this.wrapAdd(this.addSent)
-    // this.addMined = this.wrapXXX(this.addMined)
-    // this.addReverted = this.wrapXXX(this.addReverted)
-    // this.addForgotten = this.wrapXXX(this.addForgotten)
-    // this.addFinalized = this.wrapXXX(this.addFinalized)
+    this.addMined = this.wrapAdd(this.addMined)
+    this.addReverted = this.wrapAdd(this.addReverted)
+    this.addForgotten = this.wrapAdd(this.addForgotten)
+    this.addIncluded = this.wrapAdd(this.addIncluded)
     this.findByTransactionHash = this.wrapFind(this.findByTransactionHash)
-    // this.getByPositionId = this.wrapXXX(this.getByPositionId)
-    // this.getByStarkKey = this.wrapXXX(this.getByStarkKey)
-    // this.getByStateUpdateId = this.wrapXXX(this.getByStateUpdateId)
-    // this.getNotMined = this.wrapXXX(this.getNotMined)
-    // this.getMinedNotFinalized = this.wrapXXX(this.getMinedNotFinalized)
+    // this.getLatest = this.wrapGet(this.getLatest)
+    // this.getByPositionId = this.wrapGet(this.getByPositionId)
+    // this.getByStarkKey = this.wrapGet(this.getByStarkKey)
+    // this.getByStateUpdateId = this.wrapGet(this.getByStateUpdateId)
+    // this.getPending = this.wrapGet(this.getPending)
+    // this.getMinedNotFinalized = this.wrapGet(this.getMinedNotFinalized)
     this.deleteAll = this.wrapDelete(this.deleteAll)
     this.deleteAfter = this.wrapDelete(this.deleteAfter)
 
@@ -36,7 +81,7 @@ export class ForcedWithdrawRepository extends BaseRepository {
   }
 
   async addSent(
-    record: ForcedWithdrawTransactionRecord & { sentAt: Timestamp }
+    record: ForcedWithdrawTransactionRecord & { timestamp: Timestamp }
   ): Promise<Hash256> {
     const knex = await this.knex()
     await knex.transaction(async (trx) => {
@@ -44,20 +89,85 @@ export class ForcedWithdrawRepository extends BaseRepository {
         .insert(toRow(record))
         .onConflict('hash')
         .ignore()
-
       await trx('forced_withdraw_statuses').insert({
         hash: record.hash.toString(),
         status: 'sent',
-        timestamp: BigInt(record.sentAt.toString()),
+        timestamp: BigInt(record.timestamp.toString()),
       })
     })
-
     return record.hash
+  }
+
+  async addMined(
+    record: ForcedWithdrawTransactionRecord & {
+      timestamp: Timestamp
+      blockNumber: number
+    }
+  ): Promise<Hash256> {
+    const knex = await this.knex()
+    await knex.transaction(async (trx) => {
+      await trx('forced_withdraw_transactions')
+        .insert(toRow(record))
+        .onConflict('hash')
+        .ignore()
+      await trx('forced_withdraw_statuses').insert({
+        hash: record.hash.toString(),
+        status: 'mined',
+        timestamp: BigInt(record.timestamp.toString()),
+        block_number: record.blockNumber,
+      })
+    })
+    return record.hash
+  }
+
+  async addReverted(status: {
+    hash: Hash256
+    timestamp: Timestamp
+    blockNumber: number
+  }): Promise<Hash256> {
+    const knex = await this.knex()
+    await knex('forced_withdraw_statuses').insert({
+      hash: status.hash.toString(),
+      status: 'reverted',
+      timestamp: BigInt(status.timestamp.toString()),
+      block_number: status.blockNumber,
+    })
+    return status.hash
+  }
+
+  async addForgotten(status: {
+    hash: Hash256
+    timestamp: Timestamp
+  }): Promise<Hash256> {
+    const knex = await this.knex()
+    await knex('forced_withdraw_statuses').insert({
+      hash: status.hash.toString(),
+      status: 'forgotten',
+      timestamp: BigInt(status.timestamp.toString()),
+    })
+    return status.hash
+  }
+
+  async addIncluded(status: {
+    hash: Hash256
+    timestamp: Timestamp
+    blockNumber: number
+    stateUpdateId: number
+  }): Promise<Hash256> {
+    const knex = await this.knex()
+    await knex('forced_withdraw_statuses').insert({
+      hash: status.hash.toString(),
+      status: 'included',
+      timestamp: BigInt(status.timestamp.toString()),
+      block_number: status.blockNumber,
+      state_update_id: status.stateUpdateId,
+    })
+    return status.hash
   }
 
   async findByTransactionHash(
     hash: Hash256
-  ): Promise<ForcedWithdrawTransactionRecord | undefined> {
+  ): Promise<ForcedWithdrawTransactionWithHistory | undefined> {
     const knex = await this.knex()
     const row = await knex('forced_withdraw_transactions')
       .where('hash', hash.toString())
@@ -65,7 +175,24 @@ export class ForcedWithdrawRepository extends BaseRepository {
     if (!row) {
       return undefined
     }
-    return toRecord(row)
+    const record = toRecord(row)
+    const [result] = await this.recordsWithHistories([record])
+    return result
+  }
+
+  private async recordsWithHistories(
+    records: ForcedWithdrawTransactionRecord[]
+  ): Promise<ForcedWithdrawTransactionWithHistory[]> {
+    const knex = await this.knex()
+    const rows = await knex('forced_withdraw_statuses')
+      .whereIn('hash', [records.map((x) => x.hash.toString())])
+      .orderBy('timestamp', 'asc')
+    return records.map((record) => ({
+      ...record,
+      history: rows
+        .filter((x) => x.hash === record.hash.toString())
+        .map(toHistoryRow),
+    }))
   }
 
   async deleteAll() {
@@ -102,5 +229,51 @@ function toRow(
     stark_key: row.starkKey.toString(),
     amount: row.amount,
     position_id: row.positionId,
+  }
+}
+
+function toHistoryRow(row: ForcedWithdrawStatusRow): ForcedWithdrawStatus {
+  switch (row.status) {
+    case 'sent':
+      return {
+        status: 'sent',
+        timestamp: Timestamp(row.timestamp),
+      }
+    case 'mined':
+      if (row.block_number == null) {
+        throw new Error('Corrupt database: block_number is null')
+      }
+      return {
+        status: 'mined',
+        timestamp: Timestamp(row.timestamp),
+        blockNumber: row.block_number,
+      }
+    case 'reverted':
+      if (row.block_number == null) {
+        throw new Error('Corrupt database: block_number is null')
+      }
+      return {
+        status: 'reverted',
+        timestamp: Timestamp(row.timestamp),
+        blockNumber: row.block_number,
+      }
+    case 'forgotten':
+      return {
+        status: 'forgotten',
+        timestamp: Timestamp(row.timestamp),
+      }
+    case 'included':
+      if (row.block_number == null) {
+        throw new Error('Corrupt database: block_number is null')
+      }
+      if (row.state_update_id == null) {
+        throw new Error('Corrupt database: state_update_id is null')
+      }
+      return {
+        status: 'included',
+        timestamp: Timestamp(row.timestamp),
+        blockNumber: row.block_number,
+        stateUpdateId: row.state_update_id,
+      }
   }
 }
