23317607c8cadc51637f936764c8b29267c4524b
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-01-19 20:09:02 +0100

    Go with a different approach

---
+++ packages/backend/src/peripherals/database/migrations/025_transactions_refactor_2.ts
@@ -0,0 +1,56 @@
+/*
+                      ====== IMPORTANT NOTICE ======
+
+DO NOT EDIT OR RENAME THIS FILE
+
+This is a migration file. Once created the file should not be renamed or edited,
+because migrations are only run once on the production server. 
+
+If you find that something was incorrectly set up in the `up` function you
+should create a new migration file that fixes the issue.
+
+*/
+
+import { Knex } from 'knex'
+import * as migration24 from './024_transactions_refactor'
+
+export async function up(knex: Knex) {
+  await migration24.down(knex)
+
+  await knex.schema.createTable('sent_transactions', (table) => {
+    table.bigIncrements('id').primary() // surrogate key
+    table.string('type').notNullable().index()
+    table.string('transaction_hash').notNullable().index()
+    table.string('stark_key').notNullable().index()
+    table.bigInteger('vault_or_position').index()
+    table.json('data').notNullable()
+    table.bigInteger('sent_timestamp').notNullable().index()
+    table.bigInteger('mined_timestamp').index()
+    table.boolean('reverted').notNullable()
+  })
+
+  await knex.schema.createTable('user_events', (table) => {
+    table.bigIncrements('id').primary() // surrogate key
+    table.string('type').notNullable().index()
+    table.string('transaction_hash').notNullable().index()
+    table.string('stark_key').notNullable().index()
+    table.bigInteger('vault_or_position').index()
+    table.json('data').notNullable()
+    table.integer('block_number').notNullable()
+    table.bigInteger('timestamp').notNullable().index()
+  })
+
+  await knex.schema.createTable('included_forced_requests', (table) => {
+    table.string('transaction_hash').primary()
+    table.integer('block_number').notNullable()
+    table.bigInteger('timestamp').notNullable()
+    table.integer('state_update_id').notNullable()
+  })
+}
+
+export async function down(knex: Knex) {
+  await migration24.up(knex)
+  await knex.schema.dropTable('sent_transactions')
+  await knex.schema.dropTable('user_events')
+  await knex.schema.dropTable('included_forced_requests')
+}
