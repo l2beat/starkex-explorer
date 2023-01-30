f40c599b4d69604e70f5e824cf593df702d47de7
Mateusz Kelner <mateuszkelner@gmail.com>
2023-01-30 10:23:54 +0100

    Create TokenRegistrationEventCollector

---
+++ packages/backend/src/core/collectors/TokenRegistrationCollector.ts
@@ -0,0 +1,67 @@
+import { EthereumAddress, Hash256 } from "@explorer/types";
+
+import { BlockRange } from "../../model";
+import { EthereumClient } from "../../peripherals/ethereum/EthereumClient";
+import { getERC20Info } from "./assetDataGetters/getERC20Info";
+import { getERC721Info } from "./assetDataGetters/getERC721Info";
+import { getERC1155Info } from "./assetDataGetters/getERC1155Info";
+import { LogTokenRegistered } from "./events";
+
+export interface TokenRegistration {
+    name: string
+    assetId: Hash256 
+}
+
+export class TokenRegistrationCollector {
+    constructor(
+        private readonly ethereumClient: EthereumClient,
+        private readonly contractAddress: EthereumAddress
+    ) {}
+
+    async collect(blockRange: BlockRange) {
+        const logs = await this.ethereumClient.getLogsInRange(blockRange, {
+            address: this.contractAddress.toString(),
+            topics: [LogTokenRegistered.topic]
+        })
+
+        const events = logs.map(async log => {
+            const event = LogTokenRegistered.parseLog(log)
+
+            const address = EthereumAddress(event.args.assetInfo.substring(10))
+            const quantum = event.args.quantum.toNumber()
+            const assetType = event.args.assetType.toString() // true only for ERC-20 and ETH
+
+            const getAssetData = async (assetSelector: string) => {
+                switch(assetSelector) {
+                    case '0x8322fff2':
+                        return {
+                            type: 'ETH',
+                            name: 'Ethereum',
+                            symbol: 'ETH',
+                            decimals: 18
+                        };
+                    case '0xf47261b0':
+                        return {type: 'ERC-20', ...await getERC20Info(address)};
+                    case '0x02571792':
+                        return {type: 'ERC-721', ...await getERC721Info(address)};
+                    case '0x3348691d':
+                        return {type: 'ERC-1155', ...getERC1155Info(address)};
+                    case '0xb8b86672':
+                        return {type: 'MINTABLE_ERC-721', ...await getERC721Info(address)};
+                    case '0x68646e2d':
+                        return {type: 'MINTABLE_ERC-20',  ...await getERC20Info(address)};
+                }
+            }
+            
+
+            return {
+                assetType,
+                address,
+                quantum,
+                ...(await getAssetData(event.args.assetInfo.substring(0, 10)))
+            }
+        })
+
+
+    }
+}
