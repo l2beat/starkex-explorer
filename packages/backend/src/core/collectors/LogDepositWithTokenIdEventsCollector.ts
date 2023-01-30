bfa5f9df23a0023d2fb6a9679f4ad1b9714d6b1d
Mateusz Kelner <mateuszkelner@gmail.com>
2023-01-30 10:22:55 +0100

    Create LogDepositWithTokenId collector

---
+++ packages/backend/src/core/collectors/LogDepositWithTokenIdEventsCollector.ts
@@ -0,0 +1,35 @@
+import { EthereumAddress } from "@explorer/types";
+
+import { BlockRange } from "../../model";
+import { EthereumClient } from "../../peripherals/ethereum/EthereumClient";
+import { LogDepositWithTokenId } from "./events";
+
+export interface DepositWithTokenId {
+    assetId: number
+    tokenId: number
+}
+
+export class LogDepositWithTokenIdEventsCollector {
+    constructor(
+        private readonly ethereumClient: EthereumClient,
+        private readonly contractAddress: EthereumAddress
+    ) {}
+
+    async collect(blockRange: BlockRange): Promise<DepositWithTokenId[]> {
+        const logs = await this.ethereumClient.getLogsInRange(blockRange, {
+            address: this.contractAddress.toString(),
+            topics: [LogDepositWithTokenId.topic]
+        })
+
+        const events = logs.map(log => {
+            const event = LogDepositWithTokenId.parseLog(log)
+            
+            return {
+                assetId: event.args.assetId.toNumber(),
+                tokenId: event.args.tokenId.toNumber()
+            }
+        })
+
+        return events
+    }
+}
