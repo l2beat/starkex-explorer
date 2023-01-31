19772a3eecc8aed4e6fce4c65641a4e997d666cb
Mateusz Kelner <mateuszkelner@gmail.com>
2023-01-31 23:11:20 +0100

    Add getERC721URI function

--- packages/backend/src/core/collectors/assetDataGetters/getERC721URI.ts
+++ packages/backend/src/core/collectors/assetDataGetters/getERC721URI.ts
@@ -3,26 +3,23 @@ import { ethers } from 'ethers'
 
 import { provider } from './provider'
 
-export const getERC721Info = async (
+export const getERC721URI = async (
   address: EthereumAddress,
+  tokenId: bigint
 ) => {
   const abi = [
-    'function name() external view returns (string _name)',
-    'function symbol() external view returns (string _symbol)',
+    'function tokenURI(uint256 _tokenId) external view returns (string)',
   ]
 
   const contract = new ethers.Contract(address.toString(), abi, provider)
 
   //TODO: Do something about the unsafe calls and assignments
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
-  const name: string = await contract.name()
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-  const symbol: string = await contract.symbol()
+  const uri: string = await contract.tokenURI(tokenId)
 
   return {
-    name,
-    symbol,
+    uri,
     contract_error: null,
   }
 }
