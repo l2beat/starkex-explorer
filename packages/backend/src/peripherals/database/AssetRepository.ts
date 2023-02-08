6c97c6b8fa14b3e0336933ca70b9620ded0f9b10
Mateusz Kelner <mateuszkelner@gmail.com>
2023-02-08 01:24:56 +0100

    Implement find methods in AssetRepository

--- packages/backend/src/peripherals/database/AssetRepository.ts
+++ packages/backend/src/peripherals/database/AssetRepository.ts
@@ -58,13 +58,23 @@ export class AssetRepository extends BaseRepository {
   async findDetailsByAssetHash(
     assetHash: AssetHash
   ): Promise<AssetDetails | undefined> {
-    throw new Error('Not implemented')
+    const knex = await this.knex()
+    const row = await knex('asset_details')
+      .select()
+      .where({ asset_hash: assetHash.toString() })
+      .first()
+    return row ? toAssetDetailsRecord(row) : undefined
   }
 
   async findRegistrationByAssetTypeHash(
     assetTypeHash: Hash256
   ): Promise<AssetRegistrationRecord | undefined> {
-    throw new Error('Not implemented')
+    const knex = await this.knex()
+    const row = await knex('asset_registrations')
+      .select()
+      .where({ asset_type_hash: assetTypeHash.toString() })
+      .first()
+    return row ? toAssetRegistrationRecord(row) : undefined
   }
 
   async deleteAll() {
@@ -166,5 +176,120 @@ function toAssetRegistrationRow(record: AssetRegistrationRecord): AssetRegistrat
     symbol: record.symbol ?? null,
     decimals: record.decimals ?? null,
     contract_error: toSerializableJson(record.contractError),
+  }
+}
+
+function toAssetDetailsRecord(row: AssetDetailsRow): AssetDetails {
+  // I am not sure about this function, it is a bit messy
+  const {
+    asset_hash,
+    asset_type_hash,
+    type,
+    quantum,
+    address,
+    name,
+    symbol,
+    decimals,
+    token_id,
+    uri,
+    minting_blob,
+    contract_error,
+  } = row
+
+  if(!address) {
+    return {
+      assetHash: AssetHash(asset_hash),
+      assetTypeHash: Hash256(asset_type_hash),
+      type: 'ETH',
+      quantum: BigInt(quantum),
+      name: 'Ethereum',
+      symbol: 'ETH',
+      contractError: Array(contract_error),
+    } 
+  }
+
+  if (token_id) {
+    if(type === 'ERC721') {
+      return {
+        assetHash: AssetHash(asset_hash),
+        assetTypeHash: Hash256(asset_type_hash),
+        type: 'ERC721',
+        quantum: BigInt(quantum),
+        address: EthereumAddress(address),
+        tokenId: BigInt(token_id),
+        name: name ?? undefined,
+        symbol: symbol ?? undefined,
+        uri: uri ?? undefined,
+        contractError: Array(contract_error),
+      }
+    }
+    return {
+      assetHash: AssetHash(asset_hash),
+      assetTypeHash: Hash256(asset_type_hash),
+      type: 'ERC1155',
+      quantum: BigInt(quantum),
+      address: EthereumAddress(address),
+      tokenId: BigInt(token_id),
+      name: name ?? undefined,
+      symbol: symbol ?? undefined,
+      uri: uri ?? undefined,
+      contractError: Array(contract_error),
+    }
+  }
+
+  
+
+  if (minting_blob) {
+    if(type === 'MINTABLE_ERC20') {
+      return {
+        assetHash: AssetHash(asset_hash),
+        assetTypeHash: Hash256(asset_type_hash),
+        type: 'MINTABLE_ERC20',
+        quantum: BigInt(quantum),
+        address: EthereumAddress(address),
+        name: name ?? undefined,
+        symbol: symbol ?? undefined,
+        decimals: decimals ?? undefined,
+        mintingBlob: minting_blob,
+        contractError: Array(contract_error),
+      }
+    }
+    return {
+      assetHash: AssetHash(asset_hash),
+      assetTypeHash: Hash256(asset_type_hash),
+      type: 'MINTABLE_ERC721',
+      quantum: BigInt(quantum),
+      address: EthereumAddress(address),
+      name: name ?? undefined,
+      symbol: symbol ?? undefined,
+      uri: uri ?? undefined,
+      mintingBlob: minting_blob,
+      contractError: Array(contract_error),
+    }
+  }
+
+  return {
+    assetHash: AssetHash(asset_hash),
+    assetTypeHash: Hash256(asset_type_hash),
+    type: 'ERC20',
+    quantum: BigInt(quantum),
+    address: EthereumAddress(address),
+    name: name ?? undefined,
+    symbol: symbol ?? undefined,
+    decimals: decimals ?? undefined,
+    contractError: Array(contract_error),
+  }
+}
+
+function toAssetRegistrationRecord(row: AssetRegistrationRow): AssetRegistrationRecord {
+  return {
+    assetTypeHash: Hash256(row.asset_type_hash),
+    type: row.type as AssetType,
+    quantum: BigInt(row.quantum),
+    address: row.address ? EthereumAddress(row.address) : undefined,
+    name: row.name ?? undefined,
+    symbol: row.symbol ?? undefined,
+    decimals: row.decimals ?? undefined,
+    contractError: Array(row.contract_error),
   }
 }
