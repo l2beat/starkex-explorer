d8b01a21a83545468f52b074ccbaa1cbad3827ce
Mateusz Kelner <mateuszkelner@gmail.com>
2023-02-11 20:50:10 +0100

    Create EthereumTransactionTable

---
+++ packages/frontend/src/pages/newUser/EthereumTransactionsTable.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { NewTable } from "../common/table";
+import { EthereumTransactionStatus } from "./EthereumTransactionStatus";
+import { EthereumTransactionEntry } from "./UserProps";
+
+export interface EthereumTransactionsTableProps {
+    readonly ethereumTransactions: readonly EthereumTransactionEntry[];
+}
+
+export function EthereumTransactionsTable({ethereumTransactions}: EthereumTransactionsTableProps) {
+    return(<NewTable pageSize={6} id='test' title="Ethereum transactions" noRowsText="You have no ethereum transactions" columns={[{header: 'TIME'}, {header: 'HASH'}, {header: 'ASSET'}, {header: 'AMOUNT'}, {header: "STATUS"}, {header: 'TYPE'}]} 
+        rows={ethereumTransactions.map((transaction) => {
+            const link = `/ethereumTransactions/${transaction.hash}` //TODO: Construct a proper link
+            const date = new Date(transaction.timestamp.valueOf())
+            return {
+                link,
+                cells: [
+                    `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`,
+                    <a href={`etherscan link`} className='text-blue-300 underline'>{transaction.hash.substring(0, 7)}...</a>,
+                    transaction.asset,
+                    transaction.amount.toString(),
+                    <EthereumTransactionStatus status={transaction.status} />,
+                    transaction.type
+                ]
+            }
+        })}
+    />)
+}
