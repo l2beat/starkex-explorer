04aeb79727dab637bc2d5974c3d376201bfcc4c1
Mateusz Kelner <mateuszkelner@gmail.com>
2023-02-11 17:50:36 +0100

    Definitely finish AssetsTable

--- packages/frontend/src/pages/newUser/AssetsTable.tsx
+++ packages/frontend/src/pages/newUser/AssetsTable.tsx
@@ -9,7 +9,7 @@ export interface AssetsTableProps {
 }
 
 export function AssetsTable({assets}: AssetsTableProps) {
-    return(<NewTable pageSize={6} title="Assets" noRowsText="You have no assets" columns={[{header: '', className: '!w-9'}, {header: 'NAME'}, {header: 'BALANCE/ID'}, {header: 'VAULT'}, {header: "ACTION", numeric: true}]} 
+    return(<NewTable fullBackground pageSize={6} id='test' title="Assets" noRowsText="You have no assets" columns={[{header: '', className: '!w-9', textAlignClass: 'text-right'}, {header: 'NAME'}, {header: 'BALANCE/ID'}, {header: 'VAULT'}, {header: "ACTION", textAlignClass: 'text-left', className: '!w-[130px]'}]} 
         rows={assets.map((asset) => {
             const link = `/assets/${asset.name}` //TODO: Construct a proper link
             return {
