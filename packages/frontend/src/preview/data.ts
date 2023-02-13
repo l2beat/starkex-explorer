0488746ec2ecd89f4466b72b43f549fec7e131d8
Mateusz Kelner <mateuszkelner@gmail.com>
2023-02-12 21:25:23 -0500

    Add dummy data for user page

--- packages/frontend/src/preview/data.ts
+++ packages/frontend/src/preview/data.ts
@@ -19,6 +19,7 @@ import {
   StateUpdatesIndexProps,
 } from '../pages'
 import { ForcedTransactionEntry } from '../pages/forced-transactions/ForcedTransactionsIndexProps'
+import { UserProps } from '../pages/newUser'
 import { NotFoundProps } from '../pages/not-found/NotFoundProps'
 import { ForcedTradeOffersIndexProps } from '../pages/offers/ForcedTradeOffersIndexProps'
 import { TransactionFormProps } from '../pages/transaction-form'
@@ -101,6 +102,72 @@ export const HOME_PROPS: HomeProps = {
   forcedTradeOffers: createFakeOffers(10),
   totalPositions: 45762n,
   totalUpdates: 5143n,
+}
+
+export const USER_PROPS: UserProps = {
+  account: undefined,
+  withdrawableAssets: [
+    { icon: 'Icon', symbol: 'BTC', amount: 5 as unknown as bigint },
+  ],
+  offersToAccept: [
+    {
+      timestamp: Timestamp.now(),
+      asset: 'BTC',
+      assetIcon: '',
+      amount: 3 as unknown as bigint,
+      price: 17 as unknown as bigint,
+      status: 'CREATED',
+      type: 'BUY',
+    },
+  ],
+  assets: [
+    {
+      icon: 'Icon',
+      name: 'Bitcoin',
+      symbol: 'BTC',
+      balance: 1 as unknown as bigint,
+      value: 2 as unknown as bigint,
+      vaultId: 17,
+      action: 'CLOSE',
+    },
+  ],
+  totalAssets: 1 as unknown as bigint,
+  balanceChanges: [
+    {
+      timestamp: Timestamp.now(),
+      stateUpdateId: 27,
+      asset: 'BTC',
+      assetIcon: 'I',
+      newBalance: 1 as unknown as bigint,
+      change: 2 as unknown as bigint,
+      vaultId: 17,
+    },
+  ],
+  totalBalanceChanges: 1 as unknown as bigint,
+  ethereumTransactions: [
+    {
+      timestamp: Timestamp.now(),
+      hash: '0x63427846783fjhsgdgfuyt2',
+      asset: 'BTC',
+      amount: 1 as unknown as bigint,
+      assetIcon: '',
+      status: 'MINED (2/3)',
+      type: 'Forced sell',
+    },
+  ],
+  totalEthereumTransactions: 1 as unknown as bigint,
+  offers: [
+    {
+      timestamp: Timestamp.now(),
+      asset: 'BTC',
+      assetIcon: '',
+      amount: 3 as unknown as bigint,
+      price: 17 as unknown as bigint,
+      status: 'CREATED',
+      type: 'BUY',
+    },
+  ],
+  totalOffers: 1 as unknown as bigint
 }
 
 export const STATE_CHANGE_DETAILS_PROPS: StateUpdateDetailsProps = {
