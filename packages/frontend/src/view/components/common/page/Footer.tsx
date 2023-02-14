e565d0ccc59a1282f549c5334f98a4507a2bbb65
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-02-14 11:21:31 +0100

    remove @ symbol from footer

--- packages/frontend/src/view/components/common/page/Footer.tsx
+++ packages/frontend/src/view/components/common/page/Footer.tsx
@@ -11,7 +11,7 @@ export function Footer() {
         funded by
         <StarkWareLogo height={18} width={98} className="ml-1" />
       </span>
-      <span className="text-zinc-500">@Copyright 2023 L2BEAT</span>
+      <span className="text-zinc-500">Copyright 2023 L2BEAT</span>
     </footer>
   )
 }
