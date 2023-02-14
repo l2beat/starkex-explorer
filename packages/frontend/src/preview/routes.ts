4b92f29aa4c55f920a52ea2ad6e0a8e570dfd64e
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-02-14 10:14:09 +0100

    wrap HomePage with Page component

--- packages/frontend/src/preview/routes.ts
+++ packages/frontend/src/preview/routes.ts
@@ -1,10 +1,28 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import { EthereumAddress } from '@explorer/types'
 import Router from '@koa/router'
+import Koa from 'koa'
 
 import { renderHomePage } from '../view'
 
 export const router = new Router()
 
 router.get('/', (ctx) => {
-  ctx.body = renderHomePage({ title: 'foo' })
+  const account = getAccount(ctx)
+  ctx.body = renderHomePage({ title: 'foo', account })
 })
+
+function getAccount(ctx: Koa.Context) {
+  const cookie = ctx.cookies.get('account')
+  if (cookie) {
+    try {
+      return {
+        address: EthereumAddress(cookie),
+        positionId: 123n,
+        hasUpdates: Math.random() < 0.5,
+      }
+    } catch {
+      return
+    }
+  }
+}
