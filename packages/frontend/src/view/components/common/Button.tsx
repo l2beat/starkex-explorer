698bb983eb20df130edbf4c938f96f6014e2f5ad
Mateusz Kelner <mateuszkelner@gmail.com>
2023-02-15 10:02:55 -0500

    Fix minor code review issues

--- packages/frontend/src/view/components/common/Button.tsx
+++ packages/frontend/src/view/components/common/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   readonly variant?: ButtonVariant
 }
 
-const variantMap: Record<ButtonVariant, string> = {
+const classNameMap: Record<ButtonVariant, string> = {
   CONTAINED: 'mx-auto w-32 rounded bg-brand py-2 text-sm',
   OUTLINED:
     'rounded-lg border border-solid border-brand py-3 px-8 text-base hover:bg-opacity-20',
@@ -25,7 +25,7 @@ export function Button({
       onClick={onClick}
       className={cx(
         '!m-0 cursor-pointer font-semibold text-white',
-        variantMap[variant],
+        classNameMap[variant],
         className
       )}
       {...rest}
