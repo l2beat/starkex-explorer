80dd7cf92756ba14026c56a21953af33065de5a0
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-02-16 12:37:51 +0100

    create constants file

--- packages/frontend/src/preview/index.ts
+++ packages/frontend/src/preview/index.ts
@@ -1,21 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Koa from 'koa'
 import serve from 'koa-static'
+import { USE_NEW_DESIGN } from '../utils/constants'
 
 import { router as oldRouter } from './oldRoutes'
 import { router } from './routes'
 
 const app = new Koa()
 
-const IS_OLD = false
-
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-if (IS_OLD) {
-  app.use(oldRouter.routes())
-  app.use(oldRouter.allowedMethods())
-} else {
+if (USE_NEW_DESIGN) {
   app.use(router.routes())
   app.use(router.allowedMethods())
+} else {
+  app.use(oldRouter.routes())
+  app.use(oldRouter.allowedMethods())
 }
 
 app.use(serve('build/static'))
