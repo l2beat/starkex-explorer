697811f1332ac876e23cf9b3c2afb78f0c2af542
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-02-18 21:45:36 +0100

    Handle 0 items in table

--- packages/frontend/src/view/components/common/table/TablePreview.tsx
+++ packages/frontend/src/view/components/common/table/TablePreview.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react'
+import { formatInt } from '../../../../utils/formatting/formatAmount'
 
 import { LinkButton } from '../Button'
 
@@ -18,19 +19,26 @@ export function TablePreview(props: TablePreviewProps) {
       <div className="mb-5 flex items-baseline justify-between">
         <h2 className="text-xl font-semibold">{props.title}</h2>
         <p className="text-sm font-medium text-zinc-500">
-          You are viewing {props.visible} out of{' '}
+          You are viewing {formatInt(props.visible)} out of{' '}
           <a className="text-blue-600 underline" href={props.link}>
-            {props.total}
+            {formatInt(props.total)}
           </a>{' '}
           {props.entryShortNamePlural}
         </p>
       </div>
       {props.children}
-      <div className="mt-6 flex items-center justify-center">
-        <LinkButton variant="outlined" href={props.link}>
-          View all {props.entryLongNamePlural}
-        </LinkButton>
-      </div>
+      {props.visible === 0 && (
+        <div className="flex h-10 items-center justify-center text-center text-md text-zinc-500">
+          There are no {props.entryLongNamePlural} to view.
+        </div>
+      )}
+      {props.total > props.visible && (
+        <div className="mt-6 flex items-center justify-center">
+          <LinkButton variant="outlined" href={props.link}>
+            View all {props.entryLongNamePlural}
+          </LinkButton>
+        </div>
+      )}
     </section>
   )
 }
--- packages/frontend/src/view/components/common/table/TableWithPagination.tsx
+++ packages/frontend/src/view/components/common/table/TableWithPagination.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react'
+import { formatInt } from '../../../../utils/formatting/formatAmount'
 
 export interface TableWithPaginationProps {
   title: ReactNode
@@ -13,16 +14,32 @@ export interface TableWithPaginationProps {
 }
 
 export function TableWithPagination(props: TableWithPaginationProps) {
+  const start = formatInt(1 + props.offset)
+  const end = formatInt(props.offset + props.visible)
+  const total = formatInt(props.total)
   return (
     <section>
       <div className="mb-5 flex items-baseline justify-between">
         <h1 className="text-xl font-semibold">{props.title}</h1>
         <p className="text-sm font-medium text-zinc-500">
-          You are viewing {1 + props.offset}-{props.offset + props.visible} out
-          of {props.total} {props.entryShortNamePlural}
+          {props.visible !== 0 ? (
+            <>
+              You are viewing {start}-{end} out of {total}{' '}
+              {props.entryShortNamePlural}
+            </>
+          ) : (
+            <>
+              You are viewing 0 out of {total} {props.entryShortNamePlural}
+            </>
+          )}
         </p>
       </div>
       {props.children}
+      {props.visible === 0 && (
+        <div className="flex h-10 items-center justify-center text-center text-md text-zinc-500">
+          There are no {props.entryLongNamePlural} to view.
+        </div>
+      )}
     </section>
   )
 }
