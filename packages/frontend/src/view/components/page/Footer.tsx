89fa219cf1aafa1f54ef033554a314be1f43687e
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-02-19 14:48:16 +0100

    Make footer responsive

--- packages/frontend/src/view/components/page/Footer.tsx
+++ packages/frontend/src/view/components/page/Footer.tsx
@@ -5,20 +5,16 @@ import { StarkWareLogo } from '../../assets/logos/StarkWareLogo'
 
 export function Footer() {
   return (
-    <footer className="flex h-16 flex-wrap items-center	justify-between gap-y-3 whitespace-normal border-t border-t-zinc-800 p-6 text-sm">
-      <span className="flex">
+    <footer className="flex flex-wrap items-baseline	justify-between gap-y-3 whitespace-normal border-t border-t-zinc-800 p-6 text-sm">
+      <span>
         Built by{' '}
         <L2BeatLogo
           height={18}
           width={44}
-          className="relative top-[-3px] mx-1"
+          className="relative top-[-2px] mx-1 inline-block"
         />{' '}
         team and funded by
-        <StarkWareLogo
-          height={18}
-          width={98}
-          className="relative top-[-1px] ml-1"
-        />
+        <StarkWareLogo height={18} width={98} className="ml-1 inline-block" />
       </span>
       <span className="text-zinc-500">
         Copyright {new Date().getFullYear()} L2BEAT
