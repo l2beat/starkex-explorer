60c76302d17e5107a37c22defd23024c26076ed9
Adrian Adamiak <adrian@adamiak.net>
2023-02-22 11:38:47 +0000

    Add test for WithdrawableAssetRepository

--- packages/backend/src/peripherals/database/WithdrawableAssetRepository.ts
+++ packages/backend/src/peripherals/database/WithdrawableAssetRepository.ts
@@ -1,12 +1,25 @@
-import { Hash256, Timestamp } from '@explorer/types'
+import { AssetHash, Hash256, StarkKey, Timestamp } from '@explorer/types'
+import { WithdrawableAssetRow } from 'knex/types/tables'
 
 import { Logger } from '../../tools/Logger'
 import { BaseRepository } from './shared/BaseRepository'
 import { Database } from './shared/Database'
 import {
+  decodeWithdrawableBalanceChangeData,
   encodeWithdrawableBalanceChangeData,
   WithdrawableBalanceChangeData,
 } from './WithdrawableBalanceChange'
+
+export interface WithdrawalBalanceChangeRecord {
+  id?: number
+  starkKey: StarkKey
+  assetHash: AssetHash
+  balanceDelta: bigint
+  transactionHash: Hash256
+  blockNumber: number
+  timestamp: Timestamp
+  data: WithdrawableBalanceChangeData
+}
 
 export class WithdrawableAssetRepository extends BaseRepository {
   constructor(database: Database, logger: Logger) {
@@ -15,6 +28,7 @@ export class WithdrawableAssetRepository extends BaseRepository {
     /* eslint-disable @typescript-eslint/unbound-method */
 
     this.add = this.wrapAdd(this.add)
+    this.findById = this.wrapFind(this.findById)
     this.deleteAfter = this.wrapDelete(this.deleteAfter)
     this.deleteAll = this.wrapDelete(this.deleteAll)
 
@@ -44,6 +58,14 @@ export class WithdrawableAssetRepository extends BaseRepository {
     return results[0]!.id
   }
 
+  async findById(
+    id: number
+  ): Promise<WithdrawalBalanceChangeRecord | undefined> {
+    const knex = await this.knex()
+    const result = await knex('withdrawable_assets').where('id', id).first()
+    return result ? toWithdrawalBalanceChangeRecord(result) : undefined
+  }
+
   async deleteAfter(blockNumber: number): Promise<number> {
     const knex = await this.knex()
     return await knex('withdrawable_assets')
@@ -54,5 +76,19 @@ export class WithdrawableAssetRepository extends BaseRepository {
   async deleteAll(): Promise<number> {
     const knex = await this.knex()
     return await knex('withdrawable_assets').delete()
+  }
+}
+
+function toWithdrawalBalanceChangeRecord(
+  row: WithdrawableAssetRow
+): WithdrawalBalanceChangeRecord {
+  return {
+    starkKey: StarkKey(row.stark_key),
+    assetHash: AssetHash(row.asset_hash),
+    balanceDelta: row.balance_delta,
+    transactionHash: Hash256(row.transaction_hash),
+    blockNumber: row.block_number,
+    timestamp: Timestamp(row.timestamp),
+    data: decodeWithdrawableBalanceChangeData(row.event_data),
   }
 }
