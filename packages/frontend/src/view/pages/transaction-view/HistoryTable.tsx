93e962befae190b1a218f4772869219caffe58f5
Mateusz Kelner <mateuszkelner@gmail.com>
2023-02-22 22:33:59 -0500

    Create HistoryTable component

---
+++ packages/frontend/src/view/pages/transaction-view/HistoryTable.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { StatusBadge } from '../../components/StatusBadge'
+import { Table } from '../../components/table/Table'
+import { TablePreview } from '../../components/table/TablePreview'
+import { TimeCell } from '../../components/TimeCell'
+import { toStatusType } from '../user/components/UserTransactionsTable'
+import { HistoryItem } from './WithdrawalPage'
+
+interface HistoryTableProps {
+    data: HistoryItem[]
+}
+
+export function HistoryTable(props: HistoryTableProps) {
+  return (
+    <TablePreview title="History" entryShortNamePlural="History items" entryLongNamePlural="items" total={props.data.length} visible={props.data.length} link="">
+        <Table
+            columns={[
+                { header: 'Time' },
+                { header: 'Status' },
+                { header: 'Description' },
+            ]}
+            rows={props.data.map((item) => {
+                return {
+                cells: [
+                    <TimeCell timestamp={item.timestamp} />,
+                    <StatusBadge type={toStatusType(item.status)} children={item.status} />,
+                    item.description
+                ],
+                }
+            })}
+        />
+    </TablePreview>
+  )
+}
