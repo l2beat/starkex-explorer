28643c37663b4fef3a4c3624cfec8025541755a3
Mateusz Kelner <mateuszkelner@gmail.com>
2023-02-22 22:19:29 -0500

    Run yarn format:fix

--- packages/frontend/src/view/pages/transaction-view/AmountContainer.tsx
+++ packages/frontend/src/view/pages/transaction-view/AmountContainer.tsx
@@ -5,26 +5,27 @@ import { formatAmount } from '../../../utils/formatting/formatAmount'
 import { AssetWithLogo } from '../../components/AssetWithLogo'
 
 interface AmountContainerProps {
-    amount: bigint
-    asset: Asset
+  amount: bigint
+  asset: Asset
 }
 
 export function AmountContainer(props: AmountContainerProps) {
-    const assetInfo = assetToInfo(props.asset)
-    return(
-        <div className='flex items-center justify-between p-4 bg-slate-800 rounded-lg'>
-            <div className='flex flex-col gap-2'>
-                <p className='text-zinc-500 font-medium text-sm'>Amount</p>
-                <p className='text-white font-semibold text-xl'>{formatAmount(props.asset, props.amount)}</p>
-            </div>
-            <div className='flex flex-col gap-2'>
-                <p className='text-zinc-500 font-medium text-sm'>
-                    Tokens
-                </p>
-                <div className='flex items-center gap-2'>
-                    <AssetWithLogo assetInfo={assetInfo} type='symbol' /> //We should merge UserPage before so the symbol type is there
-                </div>
-            </div>
+  const assetInfo = assetToInfo(props.asset)
+  return (
+    <div className="flex items-center justify-between rounded-lg bg-slate-800 p-4">
+      <div className="flex flex-col gap-2">
+        <p className="text-sm font-medium text-zinc-500">Amount</p>
+        <p className="text-xl font-semibold text-white">
+          {formatAmount(props.asset, props.amount)}
+        </p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-sm font-medium text-zinc-500">Tokens</p>
+        <div className="flex items-center gap-2">
+          <AssetWithLogo assetInfo={assetInfo} type="symbol" /> //We should
+          merge UserPage before so the symbol type is there
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- packages/frontend/src/view/pages/transaction-view/TransactionOverview.tsx
+++ packages/frontend/src/view/pages/transaction-view/TransactionOverview.tsx
@@ -1,33 +1,50 @@
-import React from "react";
+import React from 'react'
 
-import { StatusBadge } from "../../components/StatusBadge";
-import { toStatusType } from "../user/components/UserTransactionsTable";
+import { StatusBadge } from '../../components/StatusBadge'
+import { toStatusType } from '../user/components/UserTransactionsTable'
 
 interface TransactionOverviewProps {
-    currentStatus: 'SENT (1/2)' | 'MINED (2/2)' | 'REVERTED' // TODO: Add all possible statuses
-    transactionHash: string
-    stateUpdateId: number
-    children: React.ReactNode
+  currentStatus: 'SENT (1/2)' | 'MINED (2/2)' | 'REVERTED' // TODO: Add all possible statuses
+  transactionHash: string
+  stateUpdateId: number
+  children: React.ReactNode
 }
 
 export function TransactionOverview(props: TransactionOverviewProps) {
-    return(
-        <div className="flex flex-col gap-6">
-            <p className="font-semibold text-xxl text-white">Transaction <span className="text-blue-600 underline">#{props.transactionHash.substring(0, 7)}...</span></p>
-            <div className="flex flex-col rounded-lg bg-gray-800 p-6 gap-6 items-center">
-                <div className="flex flex-col gap-2">
-                    <p className="text-zinc-500 font-semibold text-sm">Current status</p>
-                    <div className="flex items-center justify-start gap-2">
-                        <StatusBadge type={toStatusType(props.currentStatus)} children={props.currentStatus} />
-                        <p className="text-white text-lg font-semibold">Transaction included in state update <span className="underline text-blue-600">#{props.stateUpdateId}</span></p>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <p className="text-zinc-500 font-semibold text-sm">Transaction hash</p>
-                    <p className="text-blue-600 underline text-lg">{props.transactionHash}</p>
-                </div>
-                {props.children}
-            </div>
+  return (
+    <div className="flex flex-col gap-6">
+      <p className="text-xxl font-semibold text-white">
+        Transaction{' '}
+        <span className="text-blue-600 underline">
+          #{props.transactionHash.substring(0, 7)}...
+        </span>
+      </p>
+      <div className="flex flex-col items-center gap-6 rounded-lg bg-gray-800 p-6">
+        <div className="flex flex-col gap-2">
+          <p className="text-sm font-semibold text-zinc-500">Current status</p>
+          <div className="flex items-center justify-start gap-2">
+            <StatusBadge
+              type={toStatusType(props.currentStatus)}
+              children={props.currentStatus}
+            />
+            <p className="text-lg font-semibold text-white">
+              Transaction included in state update{' '}
+              <span className="text-blue-600 underline">
+                #{props.stateUpdateId}
+              </span>
+            </p>
+          </div>
         </div>
-    );
+        <div className="flex flex-col gap-2">
+          <p className="text-sm font-semibold text-zinc-500">
+            Transaction hash
+          </p>
+          <p className="text-lg text-blue-600 underline">
+            {props.transactionHash}
+          </p>
+        </div>
+        {props.children}
+      </div>
+    </div>
+  )
 }
