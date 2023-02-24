8ec1cad3f07824b2d0e7ba1fb1a140b36c698f00
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-02-24 12:50:54 +0100

    Refactor key recovery

---
+++ packages/frontend/src/scripts/keys/recovery.test.ts
@@ -0,0 +1,19 @@
+import { EthereumAddress } from '@explorer/types'
+import { expect } from 'earljs'
+
+import { starkKeyPairFromData } from './keys'
+import { signRegistration } from './recovery'
+
+describe(signRegistration.name, () => {
+  it('returns correct data', () => {
+    const pair = starkKeyPairFromData('0x1234')
+    const ethKey = EthereumAddress('0xdeadbeef12345678deadbeef12345678deadbeef')
+
+    const data = signRegistration(ethKey, pair)
+    // Tested against tenderly simulation
+    // The contract used: 0xF5C9F957705bea56a7e806943f98F7777B995826
+    expect(data.rsy).toEqual(
+      '0x06d4864a858f37e99bff58cf2f10d77dc7b295d9fdf6694b83c7a013e6b99da205bb4e4d5084c0c152be5e30350f6558dc0a450bfcea7f570aeb739cac3534f30504030787dc3978dca14f23e757e34c00b1e1122055760b5c8a1e65d1f48c6c'
+    )
+  })
+})
---
+++ packages/frontend/src/scripts/peripherals/wallet.ts
@@ -0,0 +1,60 @@
+import { Interface } from '@ethersproject/abi'
+import { EthereumAddress, StarkKey } from '@explorer/types'
+
+function getProvider() {
+  const provider = window.ethereum
+  if (!provider) {
+    throw new Error('No provider')
+  }
+  return provider
+}
+
+// #region key recovery and registration
+
+export async function signDydxKey(account: EthereumAddress): Promise<string> {
+  const message =
+    '{"types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"}],"dYdX":[{"type":"string","name":"action"},{"type":"string","name":"onlySignOn"}]},"domain":{"name":"dYdX","version":"1.0","chainId":1},"primaryType":"dYdX","message":{"action":"dYdX STARK Key","onlySignOn":"https://trade.dydx.exchange"}}'
+
+  const result = await getProvider().request({
+    method: 'eth_signTypedData_v4',
+    params: [account.toString(), message],
+  })
+  return result as string
+}
+
+export async function signDydxKeyLegacy(
+  account: EthereumAddress
+): Promise<string> {
+  const message =
+    '{\n  "name": "dYdX",\n  "version": "1.0",\n  "chainId": 1,\n  "action": "dYdX STARK Key",\n  "onlySignOn": "https://trade.dydx.exchange"\n}'
+
+  const result = await getProvider().request({
+    method: 'personal_sign',
+    // this parameter order is incorrect, but follows dYdX's implementation
+    params: [account.toString(), message],
+  })
+  return result as string
+}
+
+export async function sendRegistrationTransaction(
+  account: EthereumAddress,
+  starkKey: StarkKey,
+  registration: { rsy: string },
+  exchangeAddress: EthereumAddress
+) {
+  const coder = new Interface([
+    'function registerEthAddress(address ethKey, uint256 starkKey, bytes calldata starkSignature)',
+  ])
+  const data = coder.encodeFunctionData('registerEthAddress', [
+    account,
+    starkKey,
+    registration.rsy,
+  ])
+
+  return getProvider().request({
+    method: 'eth_sendTransaction',
+    params: [{ from: account, to: exchangeAddress, data }],
+  })
+}
+
+// #endregion
