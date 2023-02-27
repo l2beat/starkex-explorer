3deb3c58d5b114ad4576ea762dc86f858af9e99f
Adrian Adamiak <adrian@adamiak.net>
2023-02-27 18:51:01 +0000

    Enable preprocessing and new frontend on staging

--- packages/backend/src/config/environments/config.staging.ts
+++ packages/backend/src/config/environments/config.staging.ts
@@ -1,6 +1,7 @@
 import { config as dotenv } from 'dotenv'
 
 import { Config } from '../Config'
+import { getEnv } from '../getEnv'
 import { getProductionConfig } from './config.production'
 
 export function getStagingConfig(): Config {
@@ -8,5 +9,7 @@ export function getStagingConfig(): Config {
   return {
     ...getProductionConfig(),
     name: 'StarkexExplorer/Staging',
+    enablePreprocessing: getEnv.boolean('ENABLE_PREPROCESSING', true),
+    useOldFrontend: false,
   }
 }
