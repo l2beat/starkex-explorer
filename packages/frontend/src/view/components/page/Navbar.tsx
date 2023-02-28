25299618305b928fa4e86e6592f82e5b17bd0bb1
torztomasz <93620601+torztomasz@users.noreply.github.com>
2023-02-28 12:22:46 +0100

    Add gammax and myria logos (#308)
    * move icons to asset folder
    
    * add myria logo
    
    * add gammax logo
    
    * show different logo based on project
    
    * add correct process env

--- packages/frontend/src/view/components/page/Navbar.tsx
+++ packages/frontend/src/view/components/page/Navbar.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 import { JazzIcon } from '../../assets/icons/jazz/JazzIcon'
 import { DydxLogo } from '../../assets/logos/DydxLogo'
+import { GammaXLogo } from '../../assets/logos/GammaXLogo'
 import { L2BeatMinimalLogo } from '../../assets/logos/L2BeatMinimalLogo'
+import { MyriaLogo } from '../../assets/logos/MyriaLogo'
 import { Button } from '../Button'
 import { SearchBar } from '../SearchBar'
 
@@ -13,6 +15,7 @@ export interface NavbarProps {
 }
 
 export function Navbar({ user, searchBar = true }: NavbarProps) {
+  const starkExInstance = process.env.STARKEX_INSTANCE ?? 'dydx-mainnet'
   return (
     <div className="flex h-16 flex-wrap items-center justify-between gap-y-2 border-b border-zinc-800 px-6 py-2.5">
       <a
@@ -21,7 +24,15 @@ export function Navbar({ user, searchBar = true }: NavbarProps) {
       >
         <div className="flex gap-2 sm:gap-4">
           <L2BeatMinimalLogo className="h-[30px] sm:h-[36px]" />
-          <DydxLogo className="h-[26px] sm:h-8" />
+          {starkExInstance === 'dydx-mainnet' && (
+            <DydxLogo className="h-[26px] sm:h-8" />
+          )}
+          {starkExInstance === 'gammax-goerli' && (
+            <GammaXLogo className="h-[26px] sm:h-8" />
+          )}
+          {starkExInstance === 'myria-goerli' && (
+            <MyriaLogo className="h-[26px] sm:h-8" />
+          )}
         </div>
         <span className="py-1 pl-2 uppercase text-zinc-500 sm:pl-4">
           Explorer
