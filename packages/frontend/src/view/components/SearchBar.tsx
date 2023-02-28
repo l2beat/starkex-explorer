f19ecf27ec62761de420a5ae79db01a628efded8
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-02-28 09:15:32 +0100

    adjust SearchBar design

--- packages/frontend/src/view/components/SearchBar.tsx
+++ packages/frontend/src/view/components/SearchBar.tsx
@@ -9,21 +9,21 @@ export function SearchBar({ className = '' }) {
       method="GET"
       action="/search"
       className={cx(
-        'group flex h-10 w-full rounded-lg border border-gray-600 bg-transparent drop-shadow-lg focus-within:border-brand hover:border-brand',
+        'group flex h-10 w-full rounded-lg border border-gray-600 bg-transparent drop-shadow-lg focus-within:!border-brand hover:border-zinc-500',
         className
       )}
     >
       <input
-        className="w-full rounded-l-lg bg-transparent p-4 outline-0 placeholder:text-gray-600 "
+        className="w-full rounded-l-lg bg-transparent p-4 outline-0 placeholder:text-gray-600 group-focus-within:placeholder:text-zinc-500 group-hover:placeholder:text-zinc-500"
         type="text"
         placeholder="Search by Hash, Id, Public Key or Address"
         name="query"
       />
-      <button className=" flex w-12 items-center justify-center rounded-r-lg bg-transparent">
+      <button className="flex w-12 items-center justify-center rounded-r-lg bg-transparent ">
         <SearchIcon
           width={20}
           height={20}
-          className="fill-gray-600 group-focus-within:fill-white"
+          className="fill-gray-600 group-focus-within:!fill-white group-hover:fill-zinc-500"
         />
       </button>
     </form>
