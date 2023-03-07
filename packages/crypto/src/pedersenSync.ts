895e09f85bff1eb0f4be46b783f0dfaa3df4433a
Krzysztof Kaczor <krzkaczor@gmail.com>
2023-03-07 21:58:24 +0100

    Switch to new version of pedersen-fast lib

--- packages/crypto/src/pedersenSync.ts
+++ packages/crypto/src/pedersenSync.ts
@@ -1,11 +1,6 @@
 import { PedersenHash } from '@explorer/types'
-import BN from 'bn.js'
-import { pedersen } from 'pedersen-fast'
+import { pedersen_from_hex } from 'pedersen-fast'
 
 export function pedersenSync(a: PedersenHash, b: PedersenHash) {
-  // @todo avoid doing HEX->DEC conversion in JS
-  const aBN = new BN(a.toString(), 16)
-  const bBN = new BN(b.toString(), 16)
-
-  return PedersenHash(pedersen(aBN.toString(), bBN.toString()))
+  return PedersenHash(pedersen_from_hex(a.toString(), b.toString()))
 }
