8ada240f2366b93adb48cce73fb02eb12d869e78
Mateusz Kelner <mateuszkelner@gmail.com>
2023-03-22 00:58:11 +0100

    Create getWithdrawableAssetsByStarkKey method

--- packages/backend/src/peripherals/database/WithdrawableAssetRepository.ts
+++ packages/backend/src/peripherals/database/WithdrawableAssetRepository.ts
@@ -33,6 +33,7 @@ export class WithdrawableAssetRepository extends BaseRepository {
     /* eslint-disable @typescript-eslint/unbound-method */
 
     this.add = this.wrapAdd(this.add)
+    this.getWithdrawableAssetsByStarkKey = this.wrapGet(this.getWithdrawableAssetsByStarkKey)
     this.findById = this.wrapFind(this.findById)
     this.deleteAfter = this.wrapDelete(this.deleteAfter)
     this.deleteAll = this.wrapDelete(this.deleteAll)
@@ -61,6 +62,12 @@ export class WithdrawableAssetRepository extends BaseRepository {
       .returning('id')
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     return results[0]!.id
+  }
+
+  async getWithdrawableAssetsByStarkKey(starkKey: StarkKey): Promise<{assetHash: AssetHash, balanceDelta: bigint}[]> {
+    const knex = await this.knex()
+    const results: {assetHash: AssetHash, balanceDelta: bigint}[] = await knex('withdrawable_assets').select('asset_hash as assetHash').where('stark_key', starkKey.toString()).groupBy('asset_hash').sum('balance_delta as balanceDelta')
+    return results.filter((result) => result.balanceDelta > 0)
   }
 
   async findById(id: number): Promise<WithdrawableAssetRecord | undefined> {
