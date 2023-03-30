0b04fdf8cd071f32b5b57465592449277ba0d393
Tomasz Tórz <93620601+torztomasz@users.noreply.github.com>
2023-03-30 08:47:13 +0200

    Change timestamp to BigInt (#369)

--- packages/types/src/Timestamp.test.ts
+++ packages/types/src/Timestamp.test.ts
@@ -4,33 +4,52 @@ import { Timestamp } from './Timestamp'
 
 describe(Timestamp.name, () => {
   it('throws if not an integer', () => {
-    expect(() => Timestamp(3.14)).toThrow(TypeError, 'Value must be an integer')
+    expect(() => Timestamp(3.14)).toThrow(
+      RangeError,
+      'The number 3.14 cannot be converted to a BigInt because it is not an integer'
+    )
   })
 
   it('can be created from bigint', () => {
-    expect(+Timestamp(10_000_000_000n)).toEqual(10_000_000_000)
+    expect(Timestamp(10_000_000_000n).valueOf()).toEqual(10_000_000_000n)
   })
 
   it('can be created from integer', () => {
-    expect(+Timestamp(10_000_000_000)).toEqual(10_000_000_000)
+    expect(Timestamp(10_000_000_000).valueOf()).toEqual(10_000_000_000n)
   })
 
   describe(Timestamp.fromSeconds.name, () => {
     it('can be created from bigint', () => {
-      expect(+Timestamp.fromSeconds(10_000_000n)).toEqual(10_000_000_000)
+      expect(Timestamp.fromSeconds(10_000_000n).valueOf()).toEqual(
+        10_000_000_000n
+      )
     })
 
     it('can be created from integer', () => {
-      expect(+Timestamp.fromSeconds(10_000_000)).toEqual(10_000_000_000)
+      expect(Timestamp.fromSeconds(10_000_000).valueOf()).toEqual(
+        10_000_000_000n
+      )
     })
   })
 
   describe(Timestamp.fromHours.name, () => {
     it('can be created from bigint', () => {
-      expect(+Timestamp.fromHours(1n)).toEqual(3600_000)
+      expect(Timestamp.fromHours(1n).valueOf()).toEqual(3600_000n)
     })
     it('can be created from integer', () => {
-      expect(+Timestamp.fromHours(1)).toEqual(3600_000)
+      expect(Timestamp.fromHours(1).valueOf()).toEqual(3600_000n)
+    })
+  })
+
+  describe(Timestamp.toHours.name, () => {
+    it('converts timestamp to hours bigint', () => {
+      expect(Timestamp.toHours(Timestamp(7_200_000n))).toEqual(2n)
+    })
+  })
+
+  describe(Timestamp.toSeconds.name, () => {
+    it('converts timestamp to seconds bigint', () => {
+      expect(Timestamp.toSeconds(Timestamp(7_200_000n))).toEqual(7200n)
     })
   })
 })
--- packages/types/src/Timestamp.ts
+++ packages/types/src/Timestamp.ts
@@ -1,13 +1,10 @@
-export interface Timestamp extends Number {
+export interface Timestamp extends BigInt {
   _TimestampBrand: string
 }
 
-export function Timestamp(milliseconds: number | bigint) {
-  const numberMilliseconds = Number(milliseconds)
-  if (!Number.isInteger(numberMilliseconds)) {
-    throw new TypeError('Value must be an integer')
-  }
-  return numberMilliseconds as unknown as Timestamp
+export function Timestamp(milliseconds: Parameters<BigIntConstructor>[0]) {
+  const bigIntMilliseconds = BigInt(milliseconds)
+  return bigIntMilliseconds as unknown as Timestamp
 }
 
 Timestamp.now = function now() {
@@ -15,14 +12,17 @@ Timestamp.now = function now() {
 }
 
 Timestamp.fromSeconds = function fromSeconds(seconds: number | bigint) {
-  return Timestamp(Number(seconds) * 1000)
+  return Timestamp(BigInt(seconds) * 1000n)
 }
 
 Timestamp.fromHours = function fromHours(hours: number | bigint) {
-  return Timestamp(Number(hours) * 1000 * 3600)
+  return Timestamp(BigInt(hours) * 1000n * 3600n)
 }
 
 Timestamp.toHours = function toHours(timestamp: Timestamp) {
-  const milliseconds = Number(timestamp)
-  return Timestamp(milliseconds / 3600 / 1000)
+  return timestamp.valueOf() / 3600n / 1000n
+}
+
+Timestamp.toSeconds = function toSeconds(timestamp: Timestamp) {
+  return timestamp.valueOf() / 1000n
 }
