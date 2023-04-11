a33c0e706d8db6e53a5af12a3ab1bc286db74c8a
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-04-11 13:21:41 +0200

    use range function

--- packages/frontend/src/view/pages/state-update/components/StateUpdatePricesTable.tsx
+++ packages/frontend/src/view/pages/state-update/components/StateUpdatePricesTable.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import React from 'react'
 
+import { range } from 'lodash'
 import { Asset, assetToInfo } from '../../../../utils/assets'
 import { formatWithDecimals } from '../../../../utils/formatting/formatAmount'
 import { AssetWithLogo } from '../../../components/AssetWithLogo'
@@ -22,31 +23,26 @@ export function StateUpdatePricesTable(props: StateUpdatePricesTableProps) {
 
   return (
     <div className={classNames('flex', tableCount === 2 ? 'gap-12' : 'gap-6')}>
-      {Array(tableCount)
-        .fill(0)
-        .map((_, index) => {
-          return (
-            <Table
-              columns={[
-                { header: 'Asset' },
-                { header: 'Price', numeric: true },
-              ]}
-              rows={props.priceChanges
-                .filter((_, txIndex) => txIndex % tableCount === index)
-                .map((transaction) => {
-                  return {
-                    cells: [
-                      <AssetWithLogo
-                        type="small"
-                        assetInfo={assetToInfo(transaction.asset)}
-                      />,
-                      formatWithDecimals(transaction.price, 2, { prefix: '$' }),
-                    ],
-                  }
-                })}
-            />
-          )
-        })}
+      {range(tableCount).map((_, index) => {
+        return (
+          <Table
+            columns={[{ header: 'Asset' }, { header: 'Price', numeric: true }]}
+            rows={props.priceChanges
+              .filter((_, txIndex) => txIndex % tableCount === index)
+              .map((transaction) => {
+                return {
+                  cells: [
+                    <AssetWithLogo
+                      type="small"
+                      assetInfo={assetToInfo(transaction.asset)}
+                    />,
+                    formatWithDecimals(transaction.price, 2, { prefix: '$' }),
+                  ],
+                }
+              })}
+          />
+        )
+      })}
     </div>
   )
 }
