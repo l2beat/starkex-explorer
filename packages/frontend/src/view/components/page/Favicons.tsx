a66495fb6f1823b41daf5422fb18b2698f870ccd
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-04-20 08:40:04 +0200

    enable favicon

--- packages/frontend/src/view/components/page/Favicons.tsx
+++ packages/frontend/src/view/components/page/Favicons.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export function Favicons() {
   return (
     <>
-      {/* <link rel="shortcut icon" href="/favicon.png" />
+      <link rel="shortcut icon" href="/favicon.png" />
       <link rel="icon" href="/favicon.svg" />
       <link rel="apple-touch-icon" href="/favicon.png" />
-      <link rel="mask-icon" href="/mask-icon.svg" color="#000000" /> */}
+      <link rel="mask-icon" href="/mask-icon.svg" color="#000000" />
     </>
   )
 }
