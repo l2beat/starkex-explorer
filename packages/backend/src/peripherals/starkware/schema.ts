68e4f8e870820fe7aaa2f4ce16adf1a0194ad2ed
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-05-16 11:39:14 +0200

    refactor schema

--- packages/backend/src/peripherals/starkware/schema.ts
+++ packages/backend/src/peripherals/starkware/schema.ts
@@ -1,20 +1,19 @@
-import { stringAs, stringAsBigInt, stringAsInt } from '@explorer/shared'
-import { EthereumAddress } from '@explorer/types'
 import * as z from 'zod'
 
 const UnsignedIntAsString = z.string().regex(/^([1-9]\d*|0)$/)
 const SignedIntAsString = z.string().regex(/^(-?[1-9]\d*|0)$/)
 const PedersenHash = z.string().regex(/^0[a-f\d]{63}$/)
+const Bytes32 = z.string().regex(/^[a-f\d]{64}$/)
 const PedersenHash0x = z.string().regex(/^0x[a-f\d]{0,63}$/)
 const AssetHash0x = z.string().regex(/^0x[a-f\d]{0,63}$/)
 const AssetId = z.string().regex(/^0x[a-f\d]{30}$/)
+const EthereumAddress = z.string().regex(/^0x[a-fA-F0-9]{40}$/)
 
 // https://github.com/starkware-libs/starkex-data-availability-committee/blob/7d72f8e05d6d9ccda5b99444f313a7248ca479b5/src/services/perpetual/public/business_logic/state_objects.py
 export type PerpetualBatchResponse = z.infer<typeof PerpetualBatchResponse>
 export const PerpetualBatchResponse = z.strictObject({
-  update: z.union([
-    z.null(),
-    z.strictObject({
+  update: z
+    .strictObject({
       prev_batch_id: z.number(),
       position_root: PedersenHash,
       order_root: PedersenHash,
@@ -40,8 +39,8 @@ export const PerpetualBatchResponse = z.strictObject({
           fulfilled_amount: UnsignedIntAsString,
         })
       ),
-    }),
-  ]),
+    })
+    .nullable(),
 })
 
 // https://github.com/starkware-libs/starkex-data-availability-committee/blob/7d72f8e05d6d9ccda5b99444f313a7248ca479b5/src/starkware/starkware_utils/objects/starkex_state.py
@@ -77,63 +76,63 @@ const OrderType = z.enum(['LIMIT_ORDER_WITH_FEES'])
 const Signature = z.strictObject({ s: z.string(), r: z.string() })
 
 const DepositTransaction = z.strictObject({
-  position_id: stringAsBigInt(),
+  position_id: UnsignedIntAsString,
   public_key: PedersenHash0x,
-  amount: stringAsBigInt(),
+  amount: UnsignedIntAsString,
   type: z.literal('DEPOSIT'),
 })
 
 const WithdrawalToAddresTransaction = z.strictObject({
-  position_id: stringAsBigInt(),
+  position_id: UnsignedIntAsString,
   public_key: PedersenHash0x,
-  eth_address: stringAs(EthereumAddress),
-  amount: stringAsBigInt(),
-  nonce: stringAsBigInt(),
-  expiration_timestamp: stringAsBigInt(),
+  eth_address: EthereumAddress,
+  amount: UnsignedIntAsString,
+  nonce: UnsignedIntAsString,
+  expiration_timestamp: UnsignedIntAsString,
   signature: Signature,
   type: z.literal('WITHDRAWAL_TO_ADDRESS'),
 })
 
 const ForcedWithdrawalTransaction = z.strictObject({
-  position_id: stringAsBigInt(),
+  position_id: UnsignedIntAsString,
   public_key: PedersenHash0x,
-  amount: stringAsBigInt(),
+  amount: UnsignedIntAsString,
   is_valid: z.boolean(),
   type: z.literal('FORCED_WITHDRAWAL'),
 })
 
 const TradeTransaction = z.strictObject({
-  actual_b_fee: stringAsBigInt(),
-  actual_a_fee: stringAsBigInt(),
-  actual_synthetic: stringAsBigInt(),
-  actual_collateral: stringAsBigInt(),
+  actual_b_fee: UnsignedIntAsString,
+  actual_a_fee: UnsignedIntAsString,
+  actual_synthetic: UnsignedIntAsString,
+  actual_collateral: UnsignedIntAsString,
   party_b_order: z.strictObject({
-    nonce: stringAsBigInt(),
+    nonce: UnsignedIntAsString,
     is_buying_synthetic: z.boolean(),
-    expiration_timestamp: stringAsBigInt(),
+    expiration_timestamp: UnsignedIntAsString,
     signature: Signature,
-    asset_id_synthetic: z.string(),
+    asset_id_synthetic: AssetId,
     order_type: OrderType,
-    asset_id_collateral: z.string(),
-    position_id: stringAsBigInt(),
-    amount_synthetic: stringAsBigInt(),
-    amount_fee: stringAsBigInt(),
+    asset_id_collateral: AssetHash0x,
+    position_id: UnsignedIntAsString,
+    amount_synthetic: UnsignedIntAsString,
+    amount_fee: UnsignedIntAsString,
     public_key: PedersenHash0x,
-    amount_collateral: stringAsBigInt(),
+    amount_collateral: UnsignedIntAsString,
   }),
   party_a_order: z.strictObject({
-    nonce: stringAsBigInt(),
+    nonce: UnsignedIntAsString,
     is_buying_synthetic: z.boolean(),
-    expiration_timestamp: stringAsBigInt(),
+    expiration_timestamp: UnsignedIntAsString,
     signature: Signature,
-    asset_id_synthetic: z.string(),
+    asset_id_synthetic: AssetId,
     order_type: OrderType,
-    asset_id_collateral: z.string(),
-    position_id: stringAsBigInt(),
-    amount_synthetic: stringAsBigInt(),
-    amount_fee: stringAsBigInt(),
+    asset_id_collateral: AssetHash0x,
+    position_id: UnsignedIntAsString,
+    amount_synthetic: UnsignedIntAsString,
+    amount_fee: UnsignedIntAsString,
     public_key: PedersenHash0x,
-    amount_collateral: stringAsBigInt(),
+    amount_collateral: UnsignedIntAsString,
   }),
   type: z.literal('TRADE'),
 })
@@ -141,103 +140,104 @@ const TradeTransaction = z.strictObject({
 const ForcedTradeTransaction = z.strictObject({
   public_key_party_a: PedersenHash0x,
   public_key_party_b: PedersenHash0x,
-  position_id_party_a: stringAsBigInt(),
-  position_id_party_b: stringAsBigInt(),
-  collateral_asset_id: z.string(),
-  synthetic_asset_id: z.string(),
-  amount_collateral: stringAsBigInt(),
-  amount_synthetic: stringAsBigInt(),
+  position_id_party_a: UnsignedIntAsString,
+  position_id_party_b: UnsignedIntAsString,
+  collateral_asset_id: AssetHash0x,
+  synthetic_asset_id: AssetId,
+  amount_collateral: UnsignedIntAsString,
+  amount_synthetic: UnsignedIntAsString,
   is_party_a_buying_synthetic: z.boolean(),
-  nonce: stringAsBigInt(),
+  nonce: UnsignedIntAsString,
   is_valid: z.boolean(),
   type: z.literal('FORCED_TRADE'),
 })
 
 const TransferTransaction = z.strictObject({
-  amount: stringAsBigInt(),
-  nonce: stringAsBigInt(),
+  amount: UnsignedIntAsString,
+  nonce: UnsignedIntAsString,
   sender_public_key: PedersenHash0x,
-  sender_position_id: stringAsBigInt(),
+  sender_position_id: UnsignedIntAsString,
   receiver_public_key: PedersenHash0x,
-  receiver_position_id: stringAsBigInt(),
-  asset_id: z.string(),
-  expiration_timestamp: stringAsBigInt(),
+  receiver_position_id: UnsignedIntAsString,
+  // From docs: asset_id - The unique asset ID (as registered on the contract) to transfer. Currently only the collateral asset is supported.
+  asset_id: AssetHash0x,
+  expiration_timestamp: UnsignedIntAsString,
   signature: Signature,
   type: z.literal('TRANSFER'),
 })
 
 const ConditionalTransferTransaction = z.strictObject({
-  amount: stringAsBigInt(),
-  nonce: stringAsBigInt(),
+  amount: UnsignedIntAsString,
+  nonce: UnsignedIntAsString,
   sender_public_key: PedersenHash0x,
-  sender_position_id: stringAsBigInt(),
+  sender_position_id: UnsignedIntAsString,
   receiver_public_key: PedersenHash0x,
-  receiver_position_id: stringAsBigInt(),
-  asset_id: z.string(),
-  expiration_timestamp: stringAsBigInt(),
-  fact_registry_address: stringAs(EthereumAddress),
-  fact: z.string(),
+  receiver_position_id: UnsignedIntAsString,
+  // From docs: asset_id - The unique asset ID (as registered on the contract) to transfer. Currently only the collateral asset is supported.
+  asset_id: AssetHash0x,
+  expiration_timestamp: UnsignedIntAsString,
+  fact_registry_address: EthereumAddress,
+  fact: Bytes32,
   signature: Signature,
   type: z.literal('CONDITIONAL_TRANSFER'),
 })
-
 const LiquidateTransaction = z.strictObject({
   liquidator_order: z.strictObject({
     order_type: OrderType,
-    nonce: stringAsBigInt(),
+    nonce: UnsignedIntAsString,
     public_key: PedersenHash0x,
-    amount_synthetic: stringAsBigInt(),
-    amount_collateral: stringAsBigInt(),
-    amount_fee: stringAsBigInt(),
-    asset_id_synthetic: z.string(),
-    asset_id_collateral: z.string(),
-    position_id: stringAsBigInt(),
+    amount_synthetic: UnsignedIntAsString,
+    amount_collateral: UnsignedIntAsString,
+    amount_fee: UnsignedIntAsString,
+    asset_id_synthetic: AssetId,
+    asset_id_collateral: AssetHash0x,
+    position_id: UnsignedIntAsString,
     is_buying_synthetic: z.boolean(),
-    expiration_timestamp: stringAsBigInt(),
+    expiration_timestamp: UnsignedIntAsString,
     signature: Signature,
   }),
-  liquidated_position_id: stringAsBigInt(),
-  actual_collateral: stringAsBigInt(),
-  actual_synthetic: stringAsBigInt(),
-  actual_liquidator_fee: stringAsBigInt(),
+  liquidated_position_id: UnsignedIntAsString,
+  actual_collateral: UnsignedIntAsString,
+  actual_synthetic: UnsignedIntAsString,
+  actual_liquidator_fee: UnsignedIntAsString,
   type: z.literal('LIQUIDATE'),
 })
 
 const DeleverageTransaction = z.strictObject({
-  amount_collateral: stringAsBigInt(),
-  amount_synthetic: stringAsBigInt(),
-  deleveraged_position_id: stringAsBigInt(),
+  amount_collateral: UnsignedIntAsString,
+  amount_synthetic: UnsignedIntAsString,
+  deleveraged_position_id: UnsignedIntAsString,
   deleverager_is_buying_synthetic: z.boolean(),
-  deleverager_position_id: stringAsBigInt(),
+  deleverager_position_id: UnsignedIntAsString,
   synthetic_asset_id: z.string(),
   type: z.literal('DELEVERAGE'),
 })
 
 const FundingTickTransaction = z.strictObject({
   global_funding_indices: z.strictObject({
-    indices: z.record(z.string(), stringAsInt()), // value is negative but maybe we need bigint here
-    timestamp: stringAsBigInt(),
+    indices: z.record(AssetId, SignedIntAsString), // value is negative but maybe we need bigint here
+    timestamp: UnsignedIntAsString,
   }),
   type: z.literal('FUNDING_TICK'),
 })
 
 const OraclePricesTickTransaction = z.strictObject({
-  timestamp: stringAsBigInt(),
+  timestamp: UnsignedIntAsString,
   oracle_prices: z.record(
-    z.string(),
+    AssetId,
     z.strictObject({
       signed_prices: z.record(
         PedersenHash0x,
         z.strictObject({
-          external_asset_id: z.string(),
+          external_asset_id: AssetHash0x,
           timestamped_signature: z.strictObject({
-            timestamp: stringAsBigInt(),
+            timestamp: UnsignedIntAsString,
             signature: Signature,
           }),
-          price: stringAsBigInt(),
+          price: UnsignedIntAsString,
         })
       ),
-      price: stringAsBigInt(),
+      price: UnsignedIntAsString,
     })
   ),
   type: z.literal('ORACLE_PRICES_TICK'),
@@ -290,10 +290,10 @@ export type PerpetualTransactionBatchResponse = z.infer<
 export const PerpetualTransactionBatchResponse = z.strictObject({
   previous_batch_id: z.number(),
   sequence_number: z.number(),
-  previous_position_root: z.string(),
-  previous_order_root: z.string(),
-  position_root: z.string(),
-  order_root: z.string(),
+  previous_position_root: PedersenHash,
+  previous_order_root: PedersenHash,
+  position_root: PedersenHash,
+  order_root: PedersenHash,
   txs_info: z.array(TransactionInfo),
-  time_created: stringAsBigInt(),
+  time_created: UnsignedIntAsString,
 })
