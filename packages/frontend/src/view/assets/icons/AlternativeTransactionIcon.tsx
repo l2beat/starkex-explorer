7599ebb25d36cbf4ac95eb887c63384f668946c8
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-06-19 12:25:11 +0200

    remove invalid icon property

--- packages/frontend/src/view/assets/icons/AlternativeTransactionIcon.tsx
+++ packages/frontend/src/view/assets/icons/AlternativeTransactionIcon.tsx
@@ -10,7 +10,7 @@ export function AlternativeTransactionIcon(props: SVGAttributes<SVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_1877_24173)">
+      <g clipPath="url(#clip0_1877_24173)">
         <path d="M3.69976 2.3753C3.88953 2.13809 4.17684 2 4.48062 2H10V3.5V4.5V7V9.5V10.5V12H4.48062C4.17684 12 3.88953 11.8619 3.69976 11.6247L0 7L3.69976 2.3753Z" />
         <path d="M20.3002 15.6247C20.1105 15.8619 19.8232 16 19.5194 16L14 16L14 14.5L14 13.5L14 11L14 8.5L14 7.5L14 6L19.5194 6C19.8232 6 20.1105 6.13809 20.3002 6.3753L24 11L20.3002 15.6247Z" />
         <rect x="11" y="2" width="2" height="20" />
