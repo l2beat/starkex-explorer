2c7143d51d1de8538c6bb56964ab77f06c45c79c
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-07-13 08:49:48 +0200

    implement clock

---
+++ packages/backend/src/core/sync/Clock.ts
@@ -0,0 +1,29 @@
+type IntervalUnit = 's' | 'm' | 'h' | 'd'
+type Interval = `${number}${IntervalUnit}`
+
+export class Clock {
+  constructor() {}
+
+  onEvery(interval: Interval, callback: () => void) {
+    callback()
+    setInterval(() => {
+      callback()
+    }, this.toMiliseconds(interval))
+  }
+
+  toMiliseconds(interval: Interval) {
+    const unit = interval.slice(-1)
+    const number = Number(interval.slice(0, -1))
+
+    switch (unit) {
+      case 's':
+        return number * 1000
+      case 'm':
+        return number * 60 * 1000
+      case 'h':
+        return number * 60 * 60 * 1000
+      case 'd':
+        return number * 24 * 60 * 60 * 1000
+    }
+  }
+}
