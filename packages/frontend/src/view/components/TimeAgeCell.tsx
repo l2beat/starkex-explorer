f08cf7576fae4a21614708d6bc85434d267e3170
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-08-01 09:10:38 +0200

    add test for calculating age

--- packages/frontend/src/view/components/TimeAgeCell.tsx
+++ packages/frontend/src/view/components/TimeAgeCell.tsx
@@ -1,43 +1,13 @@
 import { Timestamp } from '@explorer/types'
 import React from 'react'
+import { calculateAge } from '../../utils/calculateAge'
 
 interface TimeCellProps {
   timestamp: Timestamp
 }
 
 export function TimeAgeCell({ timestamp }: TimeCellProps) {
-  const age = getAge(timestamp)
+  const age = calculateAge(timestamp)
 
   return <div className="text-xs sm:text-sm">{age}</div>
-}
-
-function getAge(timestamp: Timestamp) {
-  const now = BigInt(new Date().getTime())
-  const age = now - timestamp.valueOf()
-  const seconds = age / 1000n
-  const minutes = seconds / 60n
-  const hours = minutes / 60n
-  const days = hours / 24n
-
-  if (seconds < 10n) {
-    return 'Just now'
-  }
-
-  if (seconds < 60n && minutes === 0n && hours === 0n && days === 0n) {
-    return `${seconds} ${addPluralConditionally('sec', seconds)} ago`
-  }
-
-  if (minutes < 60 && hours === 0n && days === 0n) {
-    return `${minutes} ${addPluralConditionally('min', minutes)} ago`
-  }
-
-  if (hours < 24 && days === 0n) {
-    return `${hours} ${addPluralConditionally('hour', hours)} ago`
-  }
-
-  return `${days} ${addPluralConditionally('day', days)} ago`
-}
-
-function addPluralConditionally(val: string, n: bigint): string {
-  return n === 1n ? val : `${val}s`
 }
