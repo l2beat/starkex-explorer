dec1cf1b11038250a3bca924c3ea5ee177080f8e
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-08-04 09:01:32 +0200

    make search bar more responsive

--- packages/frontend/src/view/components/SearchBar.tsx
+++ packages/frontend/src/view/components/SearchBar.tsx
@@ -24,7 +24,7 @@ export function SearchBar({
       method="GET"
       action="/search"
       className={cx(
-        'group flex h-10 rounded-lg border border-gray-600 bg-transparent drop-shadow-lg transition-colors focus-within:!border-brand hover:border-zinc-500',
+        'group relative flex h-10 rounded-lg border border-gray-600 bg-transparent drop-shadow-lg transition-colors focus-within:!border-brand hover:border-zinc-500',
         className
       )}
     >
@@ -32,8 +32,8 @@ export function SearchBar({
         className={classNames(
           'rounded-l-lg bg-transparent outline-0 transition-[width] placeholder:text-gray-600 group-hover:placeholder:text-zinc-500',
           expandable &&
-            'w-0 py-0 pl-0 opacity-0 group-focus-within:w-40 group-focus-within:py-4 group-focus-within:pl-4 group-focus-within:opacity-100 group-focus-within:placeholder:text-zinc-500 group-hover:w-40 group-hover:py-4 group-hover:pl-4 group-hover:opacity-100',
-          !expandable && 'w-full py-4 pl-4'
+            'w-12 cursor-pointer py-0 pl-0 opacity-0 group-focus-within:mr-12 group-focus-within:w-40 group-focus-within:cursor-text group-focus-within:py-4 group-focus-within:pl-4 group-focus-within:opacity-100 group-focus-within:placeholder:text-zinc-500',
+          !expandable && 'mr-12 w-full py-4 pl-4'
         )}
         type="text"
         placeholder={expandable ? undefined : placeholder}
@@ -43,7 +43,7 @@ export function SearchBar({
       />
       <button
         className={classNames(
-          'flex w-12 items-center justify-center rounded-r-lg bg-transparent',
+          'absolute inset-y-auto right-0 flex h-10 w-12 items-center justify-center rounded-r-lg bg-transparent',
           expandable &&
             'pointer-events-none group-focus-within:pointer-events-auto'
         )}
