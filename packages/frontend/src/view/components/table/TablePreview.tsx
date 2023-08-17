5c5a4e1d57f3f4521807a713eb05e8882089f9ba
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-08-17 13:36:28 +0200

    show view all button in tabs

--- packages/frontend/src/view/components/table/TablePreview.tsx
+++ packages/frontend/src/view/components/table/TablePreview.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 
 import { ArrowRightIcon } from '../../assets/icons/ArrowIcon'
+import { Button } from '../Button'
 import { Link } from '../Link'
 import { SectionHeading } from '../SectionHeading'
 
@@ -13,27 +14,41 @@ interface TablePreviewProps {
   children: ReactNode
 }
 
-export function TablePreview(props: TablePreviewProps) {
+export function TablePreview({
+  title,
+  path,
+  entryShortNamePlural,
+  entryLongNamePlural,
+  visible,
+  children,
+}: TablePreviewProps) {
   return (
     <div>
-      {props.title && (
+      {title && (
         <SectionHeading
-          title={props.title}
+          title={title}
           description={
             <Link
               className="!gap-0.5"
-              href={props.path}
+              href={path}
               accessoryRight={<ArrowRightIcon className="scale-90" />}
             >
-              View all {props.entryShortNamePlural}{' '}
+              View all {entryShortNamePlural}
             </Link>
           }
         />
       )}
-      {props.children}
-      {props.visible === 0 && (
+      {children}
+      {visible === 0 && (
         <div className="flex h-20 items-center justify-center rounded bg-transparent text-center text-md text-zinc-500 group-[.Card]/card:bg-gray-900">
-          There are no {props.entryLongNamePlural} to view.
+          There are no {entryLongNamePlural} to view.
+        </div>
+      )}
+      {!title && (
+        <div className="mt-6 flex items-center justify-center">
+          <Button as="a" variant="outlined" href={path}>
+            View all {entryLongNamePlural}
+          </Button>
         </div>
       )}
     </div>
