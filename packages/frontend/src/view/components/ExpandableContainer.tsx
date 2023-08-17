e50900e877306e3314f32d6765cca452960ce22e
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-08-17 18:54:41 +0200

    Add expandable container to state update stats

---
+++ packages/frontend/src/view/components/ExpandableContainer.tsx
@@ -0,0 +1,49 @@
+import classNames from 'classnames'
+import React, { ReactNode } from 'react'
+
+import { ChevronDownIcon } from '../assets/icons/ChevronIcon'
+import { Button } from './Button'
+import { Card } from './Card'
+
+interface ExpandableContainerProps {
+  visible: ReactNode
+  expandedContent: ReactNode
+  className?: string
+  toggleButtonLabels?: {
+    expand: string
+    collapse: string
+  }
+}
+
+export function ExpandableContainer(props: ExpandableContainerProps) {
+  const expandLabel = props.toggleButtonLabels?.expand ?? 'Show more'
+  const collapseLabel = props.toggleButtonLabels?.collapse ?? 'Show less'
+  return (
+    <div className="ExpandableContainer">
+      <Card>
+        <div>
+          <div>{props.visible}</div>
+        </div>
+        <div
+          className={classNames(
+            'ExpandableContainer-Content max-h-0 overflow-hidden transition-max-height',
+            props.className
+          )}
+        >
+          {props.expandedContent}
+        </div>
+      </Card>
+      <div className="mt-3 flex justify-center">
+        <Button
+          variant="outlined"
+          className="ExpandableContainer-Toggle flex h-10 items-center justify-center"
+          data-expand-label={expandLabel}
+          data-collapse-label={collapseLabel}
+        >
+          <span className="ExpandableContainer-ToggleText">{expandLabel}</span>
+          <ChevronDownIcon className="ExpandableContainer-ToggleArrow inline-block transition-transform" />
+        </Button>
+      </div>
+    </div>
+  )
+}
