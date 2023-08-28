79670972b2f8946a1a84b6ddc6c0c1f07210a64f
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-08-28 11:02:48 +0200

    add to entry fn

--- packages/backend/src/core/TutorialService.ts
+++ packages/backend/src/core/TutorialService.ts
@@ -5,17 +5,18 @@ export class TutorialService {
   getTutorials(): HomeTutorialEntry[] {
     try {
       const files = fs.readdirSync('src/content/tutorials')
-
-      return files.map((filename) => {
-        const filenameWithoutExt = filename.replace('.md', '')
-        return {
-          title: filenameWithoutExt.replaceAll('-', ' '),
-          imageUrl: `/images/${filenameWithoutExt}.jpg`,
-          slug: filenameWithoutExt.toLowerCase(),
-        }
-      })
+      return files.map((filename) => this.toTutorialEntry(filename))
     } catch {
       return []
+    }
+  }
+
+  private toTutorialEntry(filename: string): HomeTutorialEntry {
+    const filenameWithoutExt = filename.replace('.md', '')
+    return {
+      title: filenameWithoutExt.replaceAll('-', ' '),
+      imageUrl: `/images/${filenameWithoutExt}.jpg`,
+      slug: filenameWithoutExt.toLowerCase(),
     }
   }
 }
