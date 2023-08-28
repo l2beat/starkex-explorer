197631525a92ec24dd63a75ab562a864c522bcf8
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-08-28 10:57:45 +0200

    fix issue with getting tutorials

--- packages/backend/src/core/TutorialService.ts
+++ packages/backend/src/core/TutorialService.ts
@@ -3,15 +3,19 @@ import fs from 'fs'
 
 export class TutorialService {
   getTutorials(): HomeTutorialEntry[] {
-    const files = fs.readdirSync('src/content/tutorials')
+    try {
+      const files = fs.readdirSync('src/content/tutorials')
 
-    return files.map((filename) => {
-      const filenameWithoutExt = filename.replace('.md', '')
-      return {
-        title: filenameWithoutExt.replaceAll('-', ' '),
-        imageUrl: `/images/${filenameWithoutExt}.jpg`,
-        slug: filenameWithoutExt.toLowerCase(),
-      }
-    })
+      return files.map((filename) => {
+        const filenameWithoutExt = filename.replace('.md', '')
+        return {
+          title: filenameWithoutExt.replaceAll('-', ' '),
+          imageUrl: `/images/${filenameWithoutExt}.jpg`,
+          slug: filenameWithoutExt.toLowerCase(),
+        }
+      })
+    } catch {
+      return []
+    }
   }
 }
