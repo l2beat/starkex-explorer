3f9e5e59038c39ca66c00d7bf4dc5fa0986912f2
Tomasz Tórz <tomasz.torz@l2beat.com>
2023-09-04 12:24:37 +0200

    remove ago from age cells

--- packages/frontend/src/utils/calculateAge.ts
+++ packages/frontend/src/utils/calculateAge.ts
@@ -13,18 +13,18 @@ export function calculateAge(timestamp: Timestamp) {
   }
 
   if (seconds < 60n && minutes === 0n && hours === 0n && days === 0n) {
-    return `${seconds} ${addPluralConditionally('sec', seconds)} ago`
+    return `${seconds} ${addPluralConditionally('sec', seconds)}`
   }
 
   if (minutes < 60 && hours === 0n && days === 0n) {
-    return `${minutes} ${addPluralConditionally('min', minutes)} ago`
+    return `${minutes} ${addPluralConditionally('min', minutes)}`
   }
 
   if (hours < 24 && days === 0n) {
-    return `${hours} ${addPluralConditionally('hour', hours)} ago`
+    return `${hours} ${addPluralConditionally('hour', hours)}`
   }
 
-  return `${days} ${addPluralConditionally('day', days)} ago`
+  return `${days} ${addPluralConditionally('day', days)}`
 }
 
 function addPluralConditionally(val: string, n: bigint): string {
