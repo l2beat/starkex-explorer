354acd5792fa61e160a2b028996cae9ee202df87
Tomasz Tórz <tomasz.torz@l2beat.com>
2024-09-05 10:20:43 +0200

    improve utils

--- packages/testescape/src/utils.ts
+++ packages/testescape/src/utils.ts
@@ -1,9 +1,8 @@
 // import KnexConstructor, { Knex } from 'knex'
-import * as helpers from '@nomicfoundation/hardhat-toolbox/network-helpers';
-import { ethers } from 'ethers';
+import * as helpers from '@nomicfoundation/hardhat-toolbox/network-helpers'
+import { ethers } from 'ethers'
 
 export class HardhatUtils {
-
   PERPETUAL_ABI = [
     'function isFrozen() public view returns (bool)',
     'function forcedWithdrawalRequest(uint256,uint256,uint256,bool) external',
@@ -11,7 +10,7 @@ export class HardhatUtils {
     'function escape(uint256,uint256,uint256) external',
     'function withdraw(uint256,uint256) external',
     'function FREEZE_GRACE_PERIOD() view returns (uint256)',
-    'event LogWithdrawalAllowed(uint256 ownerKey, uint256 assetType, uint256 nonQuantizedAmount, uint256 quantizedAmount)'
+    'event LogWithdrawalAllowed(uint256 ownerKey, uint256 assetType, uint256 nonQuantizedAmount, uint256 quantizedAmount)',
   ]
   FORCED_WITHDRAWAL
   perpetualAddress: string
@@ -25,29 +24,34 @@ export class HardhatUtils {
     }
   ) {
     this.perpetualAddress = params.perpetualAddress
-    this.FORCED_WITHDRAWAL  = {
+    this.FORCED_WITHDRAWAL = {
       positionId: 1,
       starkKey: params.starkKey,
       ethereumAddress: params.ethereumAddress,
-      quantizedAmount: 10n * (10n ** 6n)
+      quantizedAmount: 10n * 10n ** 6n,
     }
-  
+
     this.impersonateAccount(params.ethereumAddress).finally(() => {
-      this.setBalanceOf(params.ethereumAddress, 5n).finally(() => {})
+      this.setBalanceOf(params.ethereumAddress, 5n).finally(() => {
+        console.log('Finished impersonating and setting balance')
+      })
     })
   }
-
 
   async getBlockNumber() {
     return this.provider.getBlockNumber()
   }
 
   async getChainId() {
-    return this.provider.getNetwork().then(network => network.chainId)
+    return this.provider.getNetwork().then((network) => network.chainId)
   }
-  
+
   getPerpetualContract() {
-    return new ethers.Contract(this.perpetualAddress, this.PERPETUAL_ABI, this.provider)
+    return new ethers.Contract(
+      this.perpetualAddress,
+      this.PERPETUAL_ABI,
+      this.provider
+    )
   }
 
   async getTxReceipt(txId: string) {
@@ -64,24 +68,25 @@ export class HardhatUtils {
   }
 
   async setBalanceOf(address: string, ethAmount: bigint) {
-    await helpers.setBalance(address, ethAmount * (10n ** 18n))
+    await helpers.setBalance(address, ethAmount * 10n ** 18n)
   }
 
   async triggerFreezable() {
     // Forced withdrawal data (for user at Position #1)
     const premiumCost = false
-    const signer = this.provider.getSigner(this.FORCED_WITHDRAWAL.ethereumAddress); // Use the correct signer here
-    const perpetualContract = this.getPerpetualContract();
-    const perpetualContractWithSigner = perpetualContract.connect(signer);
+    const signer = this.provider.getSigner(
+      this.FORCED_WITHDRAWAL.ethereumAddress
+    ) // Use the correct signer here
+    const perpetualContract = this.getPerpetualContract()
+    const perpetualContractWithSigner = perpetualContract.connect(signer)
     await perpetualContractWithSigner.forcedWithdrawalRequest(
-      this.FORCED_WITHDRAWAL.starkKey, 
-      this.FORCED_WITHDRAWAL.positionId, 
-      this.FORCED_WITHDRAWAL.quantizedAmount, 
+      this.FORCED_WITHDRAWAL.starkKey,
+      this.FORCED_WITHDRAWAL.positionId,
+      this.FORCED_WITHDRAWAL.quantizedAmount,
       premiumCost
-    );
+    )
 
-    const freezeGracePeriod = (await perpetualContract.FREEZE_GRACE_PERIOD()).toNumber()
-    await helpers.mine(2, { interval: freezeGracePeriod })
+    await this.mineGracePeriod()
   }
 
   async mineBlock() {
@@ -89,19 +94,36 @@ export class HardhatUtils {
   }
 
   async mineGracePeriod() {
-    const perpetualContract = this.getPerpetualContract();
-    const freezeGracePeriod = (await perpetualContract.FREEZE_GRACE_PERIOD()).toNumber()
+    const perpetualContract = this.getPerpetualContract()
+    const freezeGracePeriod = (
+      await perpetualContract.FREEZE_GRACE_PERIOD()
+    ).toNumber()
     await helpers.mine(2, { interval: freezeGracePeriod })
   }
 
   async freeze() {
-    const signer = this.provider.getSigner(this.FORCED_WITHDRAWAL.ethereumAddress); // Use the correct signer here
-    const perpetualContract = this.getPerpetualContract();
-    const perpetualContractWithSigner = perpetualContract.connect(signer);
+    const signer = this.provider.getSigner(
+      this.FORCED_WITHDRAWAL.ethereumAddress
+    ) // Use the correct signer here
+    const perpetualContract = this.getPerpetualContract()
+    const perpetualContractWithSigner = perpetualContract.connect(signer)
     await perpetualContractWithSigner.freezeRequest(
-      this.FORCED_WITHDRAWAL.starkKey, 
-      this.FORCED_WITHDRAWAL.positionId, 
+      this.FORCED_WITHDRAWAL.starkKey,
+      this.FORCED_WITHDRAWAL.positionId,
       this.FORCED_WITHDRAWAL.quantizedAmount
-    );
+    )
+  }
+
+  async escape() {
+    const signer = this.provider.getSigner(
+      this.FORCED_WITHDRAWAL.ethereumAddress
+    ) // Use the correct signer here
+    const perpetualContract = this.getPerpetualContract()
+    const perpetualContractWithSigner = perpetualContract.connect(signer)
+    await perpetualContractWithSigner.escape(
+      this.FORCED_WITHDRAWAL.starkKey,
+      this.FORCED_WITHDRAWAL.positionId,
+      this.FORCED_WITHDRAWAL.quantizedAmount
+    )
   }
 }
