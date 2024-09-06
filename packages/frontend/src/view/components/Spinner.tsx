187f33fd4d7a6d2707fd6591f99968dc6a33ae2b
Tomasz Tórz <tomasz.torz@l2beat.com>
2024-09-06 08:45:04 +0200

    format fix

--- packages/frontend/src/view/components/Spinner.tsx
+++ packages/frontend/src/view/components/Spinner.tsx
@@ -5,7 +5,7 @@ export function Spinner({ className }: { className?: string }) {
   return (
     <div
       className={classNames(
-        'inline-block h-5 w-5 text-white animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white',
+        'border-current text-surface inline-block h-5 w-5 animate-spin rounded-full border-2 border-solid border-e-transparent align-[-0.125em] text-white motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white',
         className
       )}
       role="status"
