ed5b57d0c56a6c9384b01475d2732d0f1a4ac1b4
Tomasz Tórz <tomasz.torz@l2beat.com>
2024-09-09 10:48:19 +0200

    fix linter issues

--- packages/frontend/src/view/components/EtherscanLink.tsx
+++ packages/frontend/src/view/components/EtherscanLink.tsx
@@ -35,8 +35,8 @@ export function EtherscanLink(props: EtherscanLinkProps) {
 function getLink(chainId: number): `https://${string}` {
   switch (chainId) {
     case 1:
-    // Hardhat fork chainId
     case 31337:
+      // 31337 - Hardhat fork chainId
       return 'https://etherscan.io'
     case 5:
       return 'https://goerli.etherscan.io'
