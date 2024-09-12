bd42f2c86af2372618d5b4176c9b15ea24c11da8
Tomasz Tórz <tomasz.torz@l2beat.com>
2024-09-12 08:54:48 +0200

    Customize freeze banner for dYdX sunset

--- packages/frontend/src/view/components/page/FreezeBanner.tsx
+++ packages/frontend/src/view/components/page/FreezeBanner.tsx
@@ -1,7 +1,17 @@
-import { FreezeStatus } from '@explorer/shared'
+import { FreezeStatus, InstanceName } from '@explorer/shared'
 import React from 'react'
 
-export function FreezeBanner({ freezeStatus }: { freezeStatus: FreezeStatus }) {
+export function FreezeBanner({
+  instanceName,
+  freezeStatus,
+}: {
+  instanceName: InstanceName
+  freezeStatus: FreezeStatus
+}) {
+  if (instanceName === 'dYdX') {
+    return <DydxSunsetFreezeBanner freezeStatus={freezeStatus} />
+  }
+
   if (freezeStatus === 'freezable') {
     return (
       <div className="sticky top-0 z-50 flex items-center justify-center gap-4 bg-brand px-6 py-0.5 text-center text-white">
@@ -21,5 +31,41 @@ export function FreezeBanner({ freezeStatus }: { freezeStatus: FreezeStatus }) {
       </div>
     )
   }
+  return null
+}
+
+function DydxSunsetFreezeBanner({
+  freezeStatus,
+}: {
+  freezeStatus: FreezeStatus
+}) {
+  if (freezeStatus === 'freezable') {
+    return (
+      <div className="sticky top-0 z-50 flex items-center justify-center gap-4 bg-brand px-6 py-0.5 text-center text-white">
+        <span>
+          dYdX v3 is undergoing a planned sunsetting, and the operator is
+          inactive. Currently, anyone can freeze the exchange.
+        </span>
+        <a href="/freeze" className="underline">
+          Read more
+        </a>
+      </div>
+    )
+  }
+  if (freezeStatus === 'frozen') {
+    return (
+      <div className="sticky top-0 z-50 flex items-center justify-center gap-4 bg-yellow-300 px-6 py-0.5 text-center text-white">
+        <span>
+          dYdX v3 has been sunset, and the exchange contracts are frozen. Please
+          use the{' '}
+          <a href="/tutorials/escapehatch" className="underline">
+            escape hatch
+          </a>{' '}
+          functionality if you'd like to withdraw your funds.
+        </span>
+      </div>
+    )
+  }
+
   return null
 }
