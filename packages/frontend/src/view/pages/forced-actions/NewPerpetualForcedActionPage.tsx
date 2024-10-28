e9d06f77b19af1fc52bf5e9680cbd3ae45c7c570
Adrian Adamiak <adrian@adamiak.net>
2024-10-28 14:54:40 +0100

    Update dydx banner and add info on forced action pages (#529)
    * Update dydx banner
    
    * Add info panel on forced withdrawal pages
    
    * UPdate panel background

--- packages/frontend/src/view/pages/forced-actions/NewPerpetualForcedActionPage.tsx
+++ packages/frontend/src/view/pages/forced-actions/NewPerpetualForcedActionPage.tsx
@@ -6,6 +6,7 @@ import { OrderedList } from '../../components/OrderedList'
 import { ContentWrapper } from '../../components/page/ContentWrapper'
 import { Page } from '../../components/page/Page'
 import { reactToHtml } from '../../reactToHtml'
+import { DydxStoppedInfoPanel } from '../user/components/DyDxStoppedInfoPanel'
 import { getForcedActionInstructionsParams } from './components/common'
 import { FormId } from './components/form/ids'
 import { NewPerpetualForcedTradeFormContent } from './components/NewPerpetualForcedTradeFormContent'
@@ -39,50 +40,53 @@ function NewPerpetualForcedActionPage(
       description="Perform forced actions on your assets"
       context={context}
     >
-      <ContentWrapper className="grid auto-rows-min grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-12">
-        <div className="flex flex-col lg:mt-6">
-          <span className="hidden text-xl font-semibold lg:inline">
-            {instructionParams.header}
-          </span>
-          <span className="text-sm font-semibold text-zinc-500 lg:mt-3">
-            <strong>
-              The cost of this process is very high, and so should only be used
-              in an emergency.
-            </strong>{' '}
-            For regular usage, you should perform the equivalent standard
-            operation through the exchange.
-          </span>
-          <span className="mt-6 text-sm font-semibold text-zinc-500">
-            {instructionParams.description}
-          </span>
-          <OrderedList items={instructionParams.items} className="mt-3" />
+      <ContentWrapper className="flex flex-col lg:gap-12">
+        <DydxStoppedInfoPanel context={context} />
+        <div className="grid auto-rows-min grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-12">
+          <div className="flex flex-col lg:mt-6">
+            <span className="hidden text-xl font-semibold lg:inline">
+              {instructionParams.header}
+            </span>
+            <span className="text-sm font-semibold text-zinc-500 lg:mt-3">
+              <strong>
+                The cost of this process is very high, and so should only be
+                used in an emergency.
+              </strong>{' '}
+              For regular usage, you should perform the equivalent standard
+              operation through the exchange.
+            </span>
+            <span className="mt-6 text-sm font-semibold text-zinc-500">
+              {instructionParams.description}
+            </span>
+            <OrderedList items={instructionParams.items} className="mt-3" />
+          </div>
+          <Card className="row-start-1 h-min lg:col-start-2">
+            <form
+              id={FormId.Form}
+              className="flex flex-col gap-6"
+              data-props={propsJson}
+              data-user={userJson}
+              data-collateral-asset={collateralAssetJson}
+            >
+              {isWithdrawal ? (
+                <NewPerpetualForcedWithdrawalFormContent
+                  positionOrVaultId={props.positionOrVaultId}
+                  asset={props.asset}
+                  starkKey={props.starkKey}
+                  instanceName={props.context.instanceName}
+                />
+              ) : (
+                <NewPerpetualForcedTradeFormContent
+                  positionOrVaultId={props.positionOrVaultId}
+                  asset={props.asset}
+                  starkKey={props.starkKey}
+                  collateralAsset={props.context.collateralAsset}
+                  instanceName={props.context.instanceName}
+                />
+              )}
+            </form>
+          </Card>
         </div>
-        <Card className="row-start-1 h-min lg:col-start-2">
-          <form
-            id={FormId.Form}
-            className="flex flex-col gap-6"
-            data-props={propsJson}
-            data-user={userJson}
-            data-collateral-asset={collateralAssetJson}
-          >
-            {isWithdrawal ? (
-              <NewPerpetualForcedWithdrawalFormContent
-                positionOrVaultId={props.positionOrVaultId}
-                asset={props.asset}
-                starkKey={props.starkKey}
-                instanceName={props.context.instanceName}
-              />
-            ) : (
-              <NewPerpetualForcedTradeFormContent
-                positionOrVaultId={props.positionOrVaultId}
-                asset={props.asset}
-                starkKey={props.starkKey}
-                collateralAsset={props.context.collateralAsset}
-                instanceName={props.context.instanceName}
-              />
-            )}
-          </form>
-        </Card>
       </ContentWrapper>
     </Page>
   )
