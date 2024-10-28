e452212c8b8e4d00536177d01da5ad4293fca22a
Adrian Adamiak <adrian@adamiak.net>
2024-10-28 13:45:37 +0000

    Add info panel on forced withdrawal pages

---
+++ packages/frontend/src/view/pages/user/components/DyDxStoppedInfoPanel.tsx
@@ -0,0 +1,31 @@
+import { PageContext } from '@explorer/shared'
+import React from 'react'
+
+import { InfoIcon } from '../../../assets/icons/InfoIcon'
+import { Card } from '../../../components/Card'
+
+interface DydxStoppedInfoPanelProps {
+  context: PageContext
+}
+
+export function DydxStoppedInfoPanel(props: DydxStoppedInfoPanelProps) {
+  if (props.context.instanceName !== 'dYdX') {
+    return null
+  }
+  return (
+    <section>
+      <Card className="flex flex-col gap-3 bg-opacity-20">
+        <div className="flex">
+          <InfoIcon className="mr-1.5 mt-px shrink-0 fill-yellow-300" />
+          <p className="font-bold leading-tight ">
+            dYdX v3 has stopped trading.
+          </p>
+        </div>
+        <p className="mb-1.5 text-sm font-semibold leading-tight">
+          Forced withdrawals will not be executed. Withdrawals will be available
+          via Escape Hatch mechanism after October 30, ~14:30 UTC.
+        </p>
+      </Card>
+    </section>
+  )
+}
