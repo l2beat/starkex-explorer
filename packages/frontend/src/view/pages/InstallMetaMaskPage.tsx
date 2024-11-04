5c971d827c70483664f45d2fad410a1a644c3dce
Adrian Adamiak <adrian@adamiak.net>
2024-11-04 12:56:13 +0100

    Fix FAQ link on Install Metamask page (#533)

--- packages/frontend/src/view/pages/InstallMetaMaskPage.tsx
+++ packages/frontend/src/view/pages/InstallMetaMaskPage.tsx
@@ -31,7 +31,7 @@ function InstallMetaMaskPage(props: Props) {
               Even if you haven't used MetaMask to create your account on the
               exchange, you can trigger Escape Hatch operations for your
               positions using a different Ethereum account. See our{' '}
-              <Link href="/faqescapehatch#what-if-i-don-t-have-a-metamask-compatible-wallet">
+              <Link href="/tutorials/faqescapehatch#what-if-i-don-t-have-a-metamask-compatible-wallet">
                 FAQ
               </Link>{' '}
               for more details.
